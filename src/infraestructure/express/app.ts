16410639df34f465390f6cb670cbddea00c3588d
Franco de Desarrollo <francoegi01@gmail.com>
2023-10-30 19:26:50 -0300

    feat: logger income request

--- src/infraestructure/express/app.ts
+++ src/infraestructure/express/app.ts
@@ -2,6 +2,11 @@ import express from 'express'
 import { mainRouter } from './routes'
 const app = express()
 
+app.use((req, _res, next) => {
+  console.log('Request received: ', '\x1b[36m', req.method, '\x1b[35m', req.path)
+  next()
+})
+
 app.use(express.json())
 
 app.use('/api/v1', mainRouter)
