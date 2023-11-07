8046a4a2be9fb3c4ced971842b2025b976e62a43
Franco de Desarrollo <francoegi01@gmail.com>
2023-11-07 19:35:40 -0300

    refactor: app express

--- src/infraestructure/express/app.ts
+++ src/infraestructure/express/app.ts
@@ -1,27 +1,10 @@
-import express from 'express'
-import { mainRouter } from './routes'
-const app = express()
+import { type ApiExpress, createInMemoryApi } from './api-builder'
 
-app.use((req, _res, next) => {
-  console.log('Request received: ', '\x1b[36m', req.method, '\x1b[35m', req.path)
-  next()
-})
-
-app.use(express.json())
-
-app.use('/api/v1', mainRouter)
-export class ApiExpress {
-  constructor(private readonly app: ReturnType<typeof express>) {}
-  getInstance(): ReturnType<typeof express> {
-    return this.app
-  }
-
-  start(port: number | string, host: string): void {
-    this.app.listen(port, () => {
-      console.log('--------------------')
-      console.log(`[EXPRESS] Server listen on ${host}`)
-      console.log('--------------------')
-    })
+export class Applicaction {
+  private api!: ApiExpress
+  bootstrap(): ApiExpress {
+    this.api = createInMemoryApi()
+    return this.api
   }
 }
-export default new ApiExpress(app)
+export default new Applicaction().bootstrap()
