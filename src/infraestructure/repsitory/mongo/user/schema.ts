5dc779af924032cb144704d7e960db01e6440137
Franco de Desarrollo <francoegi01@gmail.com>
2023-11-19 13:50:21 -0300

    fix: :ambulance: mongo userSchema id

--- src/infraestructure/repsitory/mongo/user/schema.ts
+++ src/infraestructure/repsitory/mongo/user/schema.ts
@@ -2,6 +2,12 @@ import { type IUser } from '@/domain/user'
 import mongoose from 'mongoose'
 
 export const userSchema = new mongoose.Schema<IUser>({
+  id: {
+    type: 'string',
+    required: true,
+    unique: true,
+    index: true
+  },
   email: {
     type: 'string',
     required: true,
