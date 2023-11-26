6234d51d1ffcc3ddfe576ed25e25e01927626a11
Franco de Desarrollo <francoegi01@gmail.com>
2023-11-26 11:22:26 -0300

    feat(app): :bug: create  task

--- src/application/task/task-create.ts
+++ src/application/task/task-create.ts
@@ -5,7 +5,6 @@ export class CreateTask {
   async create(props: { task: ITaskInput; userId: IUser['id'] }): Promise<Task> {
     const task = await this.taskRepository.create(props.task)
     await this.taskRepository.setUser(task.id, props.userId)
-    task.setUser(props.userId)
     return task
   }
 }
