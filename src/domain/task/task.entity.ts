8cf6622cf907192139b6a8962a93b7449a39bfec
Franco de Desarrollo <francoegi01@gmail.com>
2023-11-26 11:22:04 -0300

    feat(domain): :bug: create  task

--- src/domain/task/task.entity.ts
+++ src/domain/task/task.entity.ts
@@ -11,15 +11,20 @@ export interface ITask {
 export type ITaskInput = Omit<ITask, 'id' | 'userId'>
 
 export class Task implements ITask {
-  id: string
-  title: string
-  description: string
+  readonly id: string
+  readonly title: string
+  readonly description: string
   userId: string | null
-  constructor({ description, title }: ITaskInput) {
-    this.id = randomUUID()
+  constructor({ description, title, id, userId }: ITask) {
+    this.id = id
     this.description = description
     this.title = title
-    this.userId = null
+    this.userId = userId
+  }
+
+  static create = (task: ITaskInput): Task => {
+    const id = randomUUID() as string
+    return new Task({ ...task, id, userId: null })
   }
 
   setUser(userId: IUser['id']): IUser['id'] {
