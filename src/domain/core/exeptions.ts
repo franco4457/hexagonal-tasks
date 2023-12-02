d617982b6baef89b8b901604bdded6b4a3bd4a17
Franco de Desarrollo <francoegi01@gmail.com>
2023-12-01 21:26:37 -0300

    feat(domain): add custom domain error and validation error

--- src/domain/core/exeptions.ts
+++ src/domain/core/exeptions.ts
@@ -1,3 +1,19 @@
-export class NotFound extends Error {
-  statusCode = 404
+export class DomainError extends Error {
+  statusCode = 500
+  constructor(message: string, statusCode?: number) {
+    super(message)
+    if (statusCode != null) this.statusCode = statusCode
+  }
+}
+
+export class ValidationError extends DomainError {
+  constructor(message: string) {
+    super(message, 400)
+  }
+}
+
+export class NotFound extends DomainError {
+  constructor(message: string) {
+    super(message, 404)
+  }
 }
