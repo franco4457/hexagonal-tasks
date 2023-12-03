48b8c7970f9ec412aa5aa13b9749b26f50ea46fa
Franco de Desarrollo <francoegi01@gmail.com>
2023-12-03 13:05:11 -0300

    feat(application): validate task on create

--- src/application/task/task-create.ts
+++ src/application/task/task-create.ts
@@ -1,9 +1,10 @@
-import { type ITaskInput, type Task, type TaskRepository } from '@/domain/task'
+import { validateTask, type ITaskInput, type Task, type TaskRepository } from '@/domain/task'
 import { type IUser } from '@/domain/user'
 export class CreateTask {
   constructor(private readonly taskRepository: TaskRepository) {}
   async create(props: { task: ITaskInput; userId: IUser['id'] }): Promise<Task> {
-    const task = await this.taskRepository.create(props.task)
+    const taskInp = await validateTask(props.task)
+    const task = await this.taskRepository.create(taskInp)
     await this.taskRepository.setUser(task.id, props.userId)
     return task
   }
