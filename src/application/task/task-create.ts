827591a3574b9bfe1c0ee508d1fc7b5292c2699b
Franco de Desarrollo <francoegi01@gmail.com>
2023-12-04 19:00:54 -0300

    feat(application): add validation id on create task

--- src/application/task/task-create.ts
+++ src/application/task/task-create.ts
@@ -1,3 +1,4 @@
+import { valdiateID } from '@/domain/core'
 import { validateTask, type ITaskInput, type Task, type TaskRepository } from '@/domain/task'
 import { type IUser } from '@/domain/user'
 export class CreateTask {
@@ -5,7 +6,8 @@ export class CreateTask {
   async create(props: { task: ITaskInput; userId: IUser['id'] }): Promise<Task> {
     const taskInp = await validateTask(props.task)
     const task = await this.taskRepository.create(taskInp)
-    await this.taskRepository.setUser(task.id, props.userId)
+    const userId = await valdiateID(props.userId, 'User ID')
+    await this.taskRepository.setUser(task.id, userId)
     return task
   }
 }
