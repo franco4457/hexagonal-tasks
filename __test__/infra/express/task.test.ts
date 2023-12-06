2b86e3c06ccf896e6507ab9fc955e2decbaec806
Franco de Desarrollo <francoegi01@gmail.com>
2023-12-06 19:24:45 -0300

    test(infra): add error case post tasks

--- __test__/infra/express/task.test.ts
+++ __test__/infra/express/task.test.ts
@@ -4,19 +4,105 @@ import api from '@/infraestructure/express/app'
 import { randomUUID } from 'node:crypto'
 const app = (await api).getInstance()
 
+const testTask = {
+  title: 'title',
+  description: 'description',
+  userId: randomUUID()
+}
+
 describe('Task', () => {
   it.concurrent('POST /task', async () => {
-    const res = await request(app).post('/api/v1/task').send({
-      title: 'title',
-      description: 'description',
-      userId: randomUUID()
-    })
+    const res = await request(app).post('/api/v1/task').send(testTask)
     expect(res.status).toBe(201)
     const { task } = res.body
     expect(task).toHaveProperty('id')
     expect(task).toHaveProperty('title')
     expect(task).toHaveProperty('description')
     expect(task).toHaveProperty('userId')
+  })
+  it.concurrent('POST /task - error missig fields', async () => {
+    const res = await request(app).post('/api/v1/task').send({})
+    expect(res.status).toBe(422)
+    expect(res.body).toEqual({
+      error: true,
+      name: 'Invalida Task',
+      issues: [
+        {
+          code: 'invalid_type',
+          expected: 'string',
+          received: 'undefined',
+          path: ['title'],
+          message: 'Title is required'
+        },
+        {
+          code: 'invalid_type',
+          expected: 'string',
+          received: 'undefined',
+          path: ['description'],
+          message: 'Description is required'
+        }
+      ],
+      errors: ['Title is required', 'Description is required'],
+      message: 'Invalida Task'
+    })
+  })
+  it.concurrent('POST /task - error invalid types', async () => {
+    const res = await request(app)
+      .post('/api/v1/task')
+      .send({
+        title: { title: 'title' },
+        description: 20,
+        userId: testTask.userId
+      })
+    expect(res.status).toBe(422)
+    expect(res.body).toEqual({
+      error: true,
+      name: 'Invalida Task',
+      issues: [
+        {
+          code: 'invalid_type',
+          expected: 'string',
+          received: 'object',
+          path: ['title'],
+          message: 'Expected string, received object'
+        },
+        {
+          code: 'invalid_type',
+          expected: 'string',
+          received: 'number',
+          path: ['description'],
+          message: 'Expected string, received number'
+        }
+      ],
+      errors: ['Expected string, received object', 'Expected string, received number'],
+      message: 'Invalida Task'
+    })
+  })
+  it.concurrent('POST /task - error invalid uuid', async () => {
+    const res = await request(app).post('/api/v1/task').send({
+      title: 'title',
+      description: 'description',
+      userId: '123'
+    })
+    expect(res.status).toBe(422)
+    console.log(res.body)
+    expect(res.body).toEqual({
+      error: true,
+      name: 'Invalid User ID',
+      issues: [
+        {
+          validation: 'uuid',
+          code: 'invalid_string',
+          message:
+            "User ID should be a valid UUID(something like this: 'string-string-string-string-string')",
+          path: []
+        }
+      ],
+      errors: [
+        "User ID should be a valid UUID(something like this: 'string-string-string-string-string')"
+      ],
+      message: 'Invalid User ID'
+    })
   })
   it.concurrent('GET /task', async () => {
     const res = await request(app).get('/api/v1/task')
