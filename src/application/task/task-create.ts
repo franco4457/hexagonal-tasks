1cfa9de49a691174108c6d58b997244921bccdf4
Franco de Desarrollo <francoegi01@gmail.com>
2023-12-06 21:39:39 -0300

    fix: validation  userId on create task

--- src/application/task/task-create.ts
+++ src/application/task/task-create.ts
@@ -5,8 +5,8 @@ export class CreateTask {
   constructor(private readonly taskRepository: TaskRepository) {}
   async create(props: { task: ITaskInput; userId: IUser['id'] }): Promise<Task> {
     const taskInp = await validateTask(props.task)
-    const task = await this.taskRepository.create(taskInp)
     const userId = await valdiateID(props.userId, 'User ID')
+    const task = await this.taskRepository.create(taskInp)
     await this.taskRepository.setUser(task.id, userId)
     return task
   }
