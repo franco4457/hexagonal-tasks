3ccba0e6b533cb93ca06463e1ed6822f2e6d1b61
Franco de Desarrollo <francoegi01@gmail.com>
2023-12-06 22:38:47 -0300

    feat(domain): aggregate user repo on task repository

--- src/domain/task/task-repository.ts
+++ src/domain/task/task-repository.ts
@@ -1,10 +1,22 @@
-import type { IUser } from '../user'
+import type { IUser, IUserRepository } from '../user'
 import type { ITask, ITaskInput, Task } from './task.entity'
 
-export interface TaskRepository {
+export interface ITaskRepository {
   getTasks: () => Promise<ITask[]>
   getTasksByUserId: (userId: IUser['id']) => Promise<ITask[]>
   getTask: (id: Task['id']) => Promise<Task>
   create: (newTask: ITaskInput) => Promise<Task>
   setUser: (id: Task['id'], userId: IUser['id']) => Promise<void>
+}
+export abstract class TaskRepository implements ITaskRepository {
+  readonly repositoryName = 'TaskRepository'
+  protected aggregates: { userRepo?: IUserRepository } = {}
+  constructor({ aggregates = {} }: { aggregates?: { userRepo?: IUserRepository } } = {}) {
+    this.aggregates = aggregates
+  }
+  abstract getTasks(): Promise<ITask[]>
+  abstract getTasksByUserId(userId: IUser['id']): Promise<ITask[]>
+  abstract getTask(id: Task['id']): Promise<Task>
+  abstract create(newTask: ITaskInput): Promise<Task>
+  abstract setUser(id: Task['id'], userId: IUser['id']): Promise<void>
 }
