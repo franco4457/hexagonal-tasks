e4d2648df3adda788f08d55f7cc5be2bf9b31546
Franco de Desarrollo <francoegi01@gmail.com>
2023-12-10 10:38:39 -0300

    feat(domain): add abstract class to userRepo

--- src/domain/user/user-repository.ts
+++ src/domain/user/user-repository.ts
@@ -1,9 +1,18 @@
-import type { IUserCreate, IUser } from './user.entity'
+import type { IUserCreate, IUser, User } from './user.entity'
 
 export interface IUserRepository {
-  getById: (id: IUser['id']) => Promise<IUser>
-  getByEmail: (email: IUser['email']) => Promise<IUser>
-  findAndValidate: (email: IUser['email'], password: string) => Promise<IUser>
-  getAll: () => Promise<IUser[]>
-  create: (user: IUserCreate) => Promise<IUser>
+  getById: (id: IUser['id']) => Promise<User>
+  getByEmail: (email: IUser['email']) => Promise<User>
+  findAndValidate: (email: IUser['email'], password: string) => Promise<User>
+  getAll: () => Promise<User[]>
+  create: (user: IUserCreate) => Promise<User>
+}
+
+export abstract class UserRepository implements IUserRepository {
+  readonly repositoryName = 'UserRepository'
+  abstract getById: (id: string) => Promise<User>
+  abstract getByEmail: (email: string) => Promise<User>
+  abstract findAndValidate: (email: string, password: string) => Promise<User>
+  abstract getAll: () => Promise<User[]>
+  abstract create: (user: IUserCreate) => Promise<User>
 }
