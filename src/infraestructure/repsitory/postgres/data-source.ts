4f366007de99260da3300089d5985d48f2d6085e
Franco de Desarrollo <francoegi01@gmail.com>
2023-12-14 21:42:50 -0300

    refactor: add barrels

---
+++ src/infraestructure/repsitory/postgres/data-source.ts
@@ -0,0 +1,16 @@
+import 'reflect-metadata'
+import { DataSource } from 'typeorm'
+import { UserEntity } from './user/entity'
+import { TaskEntity } from './task/entity'
+
+export const PostgresDataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'admin',
+  database: 'postgres',
+  synchronize: true,
+  entities: [UserEntity, TaskEntity],
+  logging: false
+})
--- src/infraestructure/repsitory/postgres/index.ts
+++ src/infraestructure/repsitory/postgres/index.ts
@@ -1,16 +1,3 @@
-import 'reflect-metadata'
-import { DataSource } from 'typeorm'
-import { UserEntity } from './user/entity'
-import { TaskEntity } from './task/entity'
-
-export const PostgresDataSource = new DataSource({
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'admin',
-  database: 'postgres',
-  synchronize: true,
-  entities: [UserEntity, TaskEntity],
-  logging: false
-})
+export * from './data-source'
+export * from './task'
+export * from './user'
