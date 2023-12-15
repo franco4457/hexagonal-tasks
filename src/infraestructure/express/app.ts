ad003af76e254bb3741871a5a16878abe9cb973f
Franco de Desarrollo <francoegi01@gmail.com>
2023-12-14 21:44:24 -0300

    feat(infra): impleent createPostgresApi

--- src/infraestructure/express/app.ts
+++ src/infraestructure/express/app.ts
@@ -1,10 +1,11 @@
 import type { ApiExpress } from './api'
-import { createInMemoryApi, createMongoApi } from './dependecies'
+import { createInMemoryApi, createMongoApi, createPostgresApi } from './dependecies'
 import { DIALECT } from '@/config'
 export class Applicaction {
   private api!: ApiExpress
   async bootstrap(): Promise<ApiExpress> {
     if (DIALECT === 'MONGODB') this.api = await createMongoApi()
+    if (DIALECT === 'POSTGRES') this.api = await createPostgresApi()
     else this.api = createInMemoryApi()
     return this.api
   }
