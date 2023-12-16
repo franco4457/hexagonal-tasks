b8272199211a160aaa9490d9206c75e22cf14874
Franco de Desarrollo <francoegi01@gmail.com>
2023-12-15 21:31:33 -0300

    fix(infra): initialize postgres to build entities

--- src/infraestructure/express/dependecies/postgres.ts
+++ src/infraestructure/express/dependecies/postgres.ts
@@ -1,4 +1,5 @@
 import {
+  PostgresDataSource,
   PostgresTaskRepository,
   PostgresUserRepository
 } from '@/infraestructure/repsitory/postgres'
@@ -6,10 +7,16 @@ import { type ApiExpress } from '../api'
 import { ApiBuilderExpress } from './api-builder'
 
 export const createPostgresApi = async (): Promise<ApiExpress> => {
-  const apiBuilder = new ApiBuilderExpress()
-  const userRepository = new PostgresUserRepository()
-  const tastRepository = new PostgresTaskRepository()
-  apiBuilder.setTaskRepository(tastRepository)
-  apiBuilder.setUserRepository(userRepository)
-  return apiBuilder.build()
+  try {
+    await PostgresDataSource.initialize()
+    const apiBuilder = new ApiBuilderExpress()
+    const userRepository = new PostgresUserRepository()
+    const tastRepository = new PostgresTaskRepository()
+    apiBuilder.setTaskRepository(tastRepository)
+    apiBuilder.setUserRepository(userRepository)
+    return apiBuilder.build()
+  } catch (error) {
+    console.log('POSTGRES INITIALIZE_ERROR', error)
+    throw error
+  }
 }
