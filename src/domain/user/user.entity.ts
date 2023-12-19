22d30d3cdcb17467dc6e4eef94abfdf138519040
Franco de Desarrollo <francoegi01@gmail.com>
2023-12-18 23:28:24 -0300

    refactor(domain): implement Entity core on user entity

--- src/domain/user/user.entity.ts
+++ src/domain/user/user.entity.ts
@@ -1,39 +1,23 @@
 import { randomUUID } from 'node:crypto'
+import { type AggregateID, Entity } from '../core'
 
-export interface IUser {
-  id: string
+export interface UserProps {
   name: string
   lastname: string
   username: string
   email: string
-}
-
-export interface IPrivateUser extends IUser {
   password: string
 }
 
-export type IUserLoginInput = Pick<IPrivateUser, 'email' | 'username' | 'password'>
+export type UserPropsLoginInput = Pick<UserProps, 'email' | 'username' | 'password'>
 
-export type IUserCreate = Pick<IUser, 'name' | 'lastname'> & IUserLoginInput
+export type UserPropsCreate = Pick<UserProps, 'name' | 'lastname'> & UserPropsLoginInput
 
-export class User implements IUser {
-  readonly id: string
-  readonly name: string
-  readonly lastname: string
-  readonly username: string
-  readonly email: string
-
-  constructor({ email, id, lastname, name, username }: IUser) {
-    this.id = id
-    this.email = email
-    this.lastname = lastname
-    this.name = name
-    this.username = username
-  }
-
-  static create(props: IUserCreate): IUser {
+export class User extends Entity<UserProps> {
+  protected readonly _id!: AggregateID
+  static create(props: UserPropsCreate): User {
     const id = randomUUID()
-    const user = new User({ ...props, id })
+    const user = new User({ props, id })
     return user
   }
 }
