4d9075c74919301ecb15c477c4d07cb636bd0c0d
Franco de Desarrollo <francoegi01@gmail.com>
2023-12-20 23:18:52 -0300

    fix(domain/user): types userExceptions

--- src/domain/user/user.exceptions.ts
+++ src/domain/user/user.exceptions.ts
@@ -1,14 +1,14 @@
 import { AlreadyExist, NotFound, ValidationError } from '@/domain/core'
-import type { IUser } from './user.entity'
+import type { User } from './user.entity'
 import { type ZodIssue } from 'zod'
 export class UserNotFound extends NotFound {
-  constructor(value: string, field: keyof IUser = 'id') {
+  constructor(value: string, field: keyof User | keyof User['props'] = 'id') {
     super(`User with ${field}: ${value} not found`)
   }
 }
 
 export class UserAlreadyExist extends AlreadyExist {
-  constructor(value: string, field: keyof IUser = 'email') {
+  constructor(value: string, field: keyof User | keyof User['props'] = 'email') {
     super(`User with ${field}: ${value} already exist`)
   }
 }
