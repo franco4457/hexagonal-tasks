0c40628772b8a0413babeceed7ed22bbbcb65b45
Franco de Desarrollo <francoegi01@gmail.com>
2023-12-20 21:56:57 -0300

    feat(domain/core): add base Value object

---
+++ src/domain/core/ddd/value-object.base.ts
@@ -0,0 +1,25 @@
+export abstract class ValueObject<T> {
+  protected readonly _value: T
+
+  constructor(value: T) {
+    this.validate(value)
+    this._value = value
+  }
+
+  public unpack(): T {
+    return this._value
+  }
+
+  protected abstract validate(value: T): boolean
+
+  static isValueObject(value: unknown): value is ValueObject<unknown> {
+    return value instanceof ValueObject
+  }
+
+  equals(valueObject?: ValueObject<T>): boolean {
+    if (valueObject == null) {
+      return false
+    }
+    return JSON.stringify(this) === JSON.stringify(valueObject)
+  }
+}
