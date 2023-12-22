6af06f66f7a8b47e8bf74f3627ccac951c88a02d
Franco de Desarrollo <francoegi01@gmail.com>
2023-12-22 19:44:04 -0300

    chore: update user barrel

--- src/domain/user/index.ts
+++ src/domain/user/index.ts
@@ -1,4 +1,7 @@
 export * from './user-repository'
+export * from './user.dto'
 export * from './user.entity'
 export * from './user.exceptions'
+export * from './user.mapper'
 export * from './user.shema'
+export * from './value-objects'
