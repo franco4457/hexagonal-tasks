d72258b97dcc180b183b3625225a08f78d303754
Franco de Desarrollo <francoegi01@gmail.com>
2023-12-24 12:11:14 -0300

    feat(application): user use cases implement refact

--- src/application/user/user-login.ts
+++ src/application/user/user-login.ts
@@ -1,10 +1,12 @@
-import type { User, IUserLoginInput, IUserRepository } from '@/domain/user'
+import type { User, UserPropsLoginInput, IUserRepository } from '@/domain/user'
 
 export class UserLogin {
   constructor(private readonly userRepository: IUserRepository) {}
 
-  async login(props: IUserLoginInput): Promise<User> {
+  async login(props: UserPropsLoginInput): Promise<User> {
     const user = await this.userRepository.findAndValidate(props.email, props.password)
+    const isValidPass = await user.getProps().password.compare(props.password)
+    if (!isValidPass) throw new Error('Invalid password')
     return user
   }
 }
--- src/application/user/user-register.ts
+++ src/application/user/user-register.ts
@@ -1,14 +1,13 @@
 import { User, validateUser } from '@/domain/user'
-import type { IUserRepository, IUser } from '@/domain/user'
+import type { IUserRepository } from '@/domain/user'
 
 export class UserRegister {
   constructor(private readonly userRepository: IUserRepository) {}
 
-  async register(props: unknown): Promise<IUser> {
+  async register(props: unknown): Promise<User> {
     const userInput = await validateUser(props)
-    const user = User.create(userInput)
-    await this.userRepository.create({ ...user, password: userInput.password })
-
+    const user = await User.create(userInput)
+    await this.userRepository.create(user)
     return user
   }
 }
