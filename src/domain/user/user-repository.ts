d490f55fe95aa127a2d4a8b4a34807ba81ed70c1
Franco de Desarrollo <francoegi01@gmail.com>
2023-12-24 11:51:15 -0300

    refactor(domain): add user mapper

--- src/domain/user/user-repository.ts
+++ src/domain/user/user-repository.ts
@@ -1,18 +1,20 @@
-import type { UserPropsCreate, User } from './user.entity'
+import type { User } from './user.entity'
+import { UserMapper } from './user.mapper'
 
 export interface IUserRepository {
   getById: (id: string) => Promise<User>
   getByEmail: (email: string) => Promise<User>
   findAndValidate: (email: string, password: string) => Promise<User>
   getAll: () => Promise<User[]>
-  create: (user: UserPropsCreate) => Promise<User>
+  create: (user: User) => Promise<User>
 }
 
 export abstract class UserRepository implements IUserRepository {
   readonly repositoryName = 'UserRepository'
+  protected readonly mapper = new UserMapper()
   abstract getById(id: string): Promise<User>
   abstract getByEmail(email: string): Promise<User>
   abstract findAndValidate(email: string, password: string): Promise<User>
   abstract getAll(): Promise<User[]>
-  abstract create(user: UserPropsCreate): Promise<User>
+  abstract create(user: User): Promise<User>
 }
