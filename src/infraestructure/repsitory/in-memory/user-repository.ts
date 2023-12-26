1997f4a76431f1df5f453974a6a873bf24e0e8de
Franco de Desarrollo <francoegi01@gmail.com>
2023-12-26 20:57:52 -0300

    refact(infra): in memory user repo pass test

--- src/infraestructure/repsitory/in-memory/user-repository.ts
+++ src/infraestructure/repsitory/in-memory/user-repository.ts
@@ -1,53 +1,56 @@
-import { type IUserCreate, UserRepository, User, type IPrivateUser } from '@/domain/user'
+import { UserRepository, type User, type UserModel } from '@/domain/user'
 import { UserAlreadyExist, UserNotFound } from '@/domain/user/user.exceptions'
 
 export class InMemoryUserRepository extends UserRepository {
-  private readonly users: IPrivateUser[] = [
+  private readonly users: UserModel[] = [
     {
       id: 'c2d7e0e0-4e0a-4b7a-8c7e-2a9a9b0a3b1a',
       email: 'example@mail.com',
       lastname: 'tester',
       name: 'test',
-      password: 'Pass1234',
-      username: 'tested'
+      password: '$2b$10$LW29SqaXA.1e/ruyZjyjNumC7cItPePd2guY9Eq3udPl62iep9l6u',
+      username: 'tested',
+      createdAt: new Date(),
+      updatedAt: new Date()
     }
   ]
 
-  constructor(users?: IPrivateUser[]) {
+  constructor(users?: UserModel[]) {
     super()
     if (users != null) this.users = users
   }
 
-  getByEmail = async (email: string): Promise<User> => {
+  async getByEmail(email: string): Promise<User> {
     const user = this.users.find((user) => user.email === email)
     if (user == null) throw new UserNotFound(email, 'email')
 
-    return new User(user)
+    return this.mapper.toDomain(user)
   }
 
-  findAndValidate = async (email: string, password: string): Promise<User> => {
-    const user = this.users.find((user) => user.email === email)
-    if (user == null) throw new UserNotFound(email, 'email')
-    if (user.password !== password) throw new UserNotFound(email, 'email')
-    return new User(user)
+  async findAndValidate(email: string, password: string): Promise<User> {
+    const record = this.users.find((user) => user.email === email)
+    if (record == null) throw new UserNotFound(email, 'email')
+    const user = this.mapper.toDomain(record)
+    const isValidPass = await user.getProps().password.compare(password)
+    if (!isValidPass) throw new UserNotFound(email, 'email')
+    return user
   }
 
-  getAll = async (): Promise<User[]> => {
-    return this.users.map((user) => new User(user))
+  async getAll(): Promise<User[]> {
+    return this.users.map((user) => this.mapper.toDomain(user))
   }
 
-  create = async (user: IUserCreate): Promise<User> => {
-    if (this.users.some((u) => u.email === user.email)) {
-      throw new UserAlreadyExist(user.email, 'email')
+  async create(user: User): Promise<User> {
+    if (this.users.some((u) => u.email === user.getProps().email)) {
+      throw new UserAlreadyExist(user.getProps().email, 'email')
     }
-    const newUser = User.create(user)
-    this.users.push({ ...newUser, password: user.password })
-    return newUser
+    this.users.push(this.mapper.toPersistence(user))
+    return user
   }
 
-  getById = async (id: string): Promise<User> => {
+  async getById(id: string): Promise<User> {
     const user = this.users.find((user) => user.id === id)
     if (user == null) throw new UserNotFound(id)
-    return user
+    return this.mapper.toDomain(user)
   }
 }
