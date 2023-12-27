11c055429dc4a712d50ed6138ff95e5fd3a676df
Franco de Desarrollo <francoegi01@gmail.com>
2023-12-27 17:34:19 -0300

    fix(infra): update in memory task repo to pass tests

--- src/infraestructure/repsitory/in-memory/task-repository.ts
+++ src/infraestructure/repsitory/in-memory/task-repository.ts
@@ -1,22 +1,24 @@
-import { Task, type ITaskInput, TaskRepository, TaskNotFound } from '@/domain/task'
-import { type IUserRepository, type IUser, UserNotFound } from '@/domain/user'
+import { type Task, TaskRepository, TaskNotFound, type TaskModel } from '@/domain/task'
+import { type IUserRepository, type User, UserNotFound } from '@/domain/user'
 
 export class InMemoryTaskRepository extends TaskRepository {
-  private readonly tasks: Task[] = []
+  private readonly tasks: TaskModel[] = []
   constructor({
     tasks = [],
     aggregates = {}
-  }: { tasks?: Task[]; aggregates?: { userRepo?: IUserRepository } } = {}) {
+  }: { tasks?: TaskModel[]; aggregates?: { userRepo?: IUserRepository } } = {}) {
     super({ aggregates })
     this.tasks = tasks
   }
 
   async getTasks(): Promise<Task[]> {
-    return this.tasks
+    return this.tasks.map((task) => this.mapper.toDomain(task))
   }
 
-  async getTasksByUserId(userId: IUser['id']): Promise<Task[]> {
-    return this.tasks.filter((task) => task.userId === userId)
+  async getTasksByUserId(userId: User['id']): Promise<Task[]> {
+    return this.tasks
+      .filter((task) => task.userId === userId)
+      .map((task) => this.mapper.toDomain(task))
   }
 
   async getTask(id: string): Promise<Task> {
@@ -24,16 +26,15 @@ export class InMemoryTaskRepository extends TaskRepository {
     if (task == null) {
       throw new TaskNotFound(id)
     }
-    return new Task(task)
+    return this.mapper.toDomain(task)
   }
 
-  async create(newTask: ITaskInput): Promise<Task> {
-    const task = Task.create(newTask)
-    this.tasks.push(task)
+  async create(task: Task): Promise<Task> {
+    this.tasks.push(this.mapper.toPersistence(task))
     return task
   }
 
-  async setUser(id: Task['id'], userId: IUser['id']): Promise<void> {
+  async setUser(id: Task['id'], userId: User['id']): Promise<void> {
     const indexTask = this.tasks.findIndex((task) => task.id === id)
     if (indexTask === -1) {
       throw new TaskNotFound(id)
@@ -43,7 +44,7 @@ export class InMemoryTaskRepository extends TaskRepository {
       if (user == null) {
         throw new UserNotFound(userId)
       }
-      this.tasks[indexTask].setUser(user.id)
+      this.tasks[indexTask].userId = userId
     } catch (error) {
       this.tasks.splice(indexTask, 1)
       throw error
