ca37960320bafa701559f91d27e6c786e52fdcd5
Franco de Desarrollo <francoegi01@gmail.com>
2023-12-29 20:49:46 -0300

    feat(domain): create task domain event

---
+++ src/domain/task/events/task-create.event.ts
@@ -0,0 +1,11 @@
+import { DomainEvent, type DomainEventProps } from '@/domain/core'
+
+export class TaskCreateEvent extends DomainEvent {
+  readonly title: string
+  readonly userId: string
+  constructor(props: DomainEventProps<TaskCreateEvent>) {
+    super(props)
+    this.title = props.title
+    this.userId = props.userId
+  }
+}
