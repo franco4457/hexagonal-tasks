d5ab0dbe26100c296d85e258619ec5f2616d4a7f
Franco de Desarrollo <francoegi01@gmail.com>
2023-12-31 12:10:26 -0300

    refacort(app): add method to get config for repos on api builder

--- src/application/api/builder.ts
+++ src/application/api/builder.ts
@@ -1,8 +1,13 @@
 import { type TaskRepository } from '@/domain/task'
 import { type IUserRepository } from '@/domain/user'
+import type EventEmitter2 from 'eventemitter2'
 
 export interface IApiBuilder {
   reset: () => IApiBuilder
+  getRepoConfig: () => {
+    eventEmitter: EventEmitter2
+    appContext?: string
+  }
   setTaskRepository: (taskRepository: TaskRepository) => IApiBuilder
   setUserRepository: (userRepository: IUserRepository) => IApiBuilder
   build: () => any
