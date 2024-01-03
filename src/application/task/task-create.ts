17a1461377b55555b6276b6435ce8c5a0549c124
Franco de Desarrollo <francoegi01@gmail.com>
2024-01-03 17:07:45 -0300

    feat(app): create multiple tasks and add trasnsacction

--- src/application/task/task-create.ts
+++ src/application/task/task-create.ts
@@ -1,16 +1,33 @@
 import { valdiateID } from '@/domain/core'
 import { validateTask, type TaskPropsCreate, Task, type TaskRepository } from '@/domain/task'
 import { type User } from '@/domain/user'
+
+interface CreateTaskProps {
+  task: Omit<TaskPropsCreate, 'userId'>
+  userId: User['id']
+}
 export class CreateTask {
   constructor(private readonly taskRepository: TaskRepository) {}
-  async create(props: {
-    task: Omit<TaskPropsCreate, 'userId'>
-    userId: User['id']
-  }): Promise<Task> {
-    const taskInp = await validateTask(props.task)
-    const userId = await valdiateID(props.userId, 'User ID')
-    const task = Task.create({ ...taskInp, userId })
-    await this.taskRepository.create(task)
-    return task
+
+  async create(props: CreateTaskProps): Promise<Task>
+  async create(props: CreateTaskProps[]): Promise<Task[]>
+  async create(props: CreateTaskProps | CreateTaskProps[]): Promise<Task | Task[]> {
+    if (Array.isArray(props)) {
+      const tasks = await Promise.all(
+        props.map(async (p) => {
+          const taskInp = await validateTask(p.task)
+          const userId = await valdiateID(p.userId, 'User ID')
+          return Task.create({ ...taskInp, userId })
+        })
+      )
+      await this.taskRepository.transaction(async () => await this.taskRepository.create(tasks))
+      return tasks
+    } else {
+      const taskInp = await validateTask(props.task)
+      const userId = await valdiateID(props.userId, 'User ID')
+      const task = Task.create({ ...taskInp, userId })
+      await this.taskRepository.transaction(async () => await this.taskRepository.create(task))
+      return task
+    }
   }
 }
