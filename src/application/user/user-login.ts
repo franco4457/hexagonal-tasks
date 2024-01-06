ec1c8bf99f745dba097c9ef6846dfae916c39826
Franco de Desarrollo <francoegi01@gmail.com>
2024-01-06 10:37:26 -0300

    fix(app): change repo type and rm extra validation

--- src/application/user/user-login.ts
+++ src/application/user/user-login.ts
@@ -1,15 +1,15 @@
 import {
   type User,
   type UserPropsLoginInput,
-  type IUserRepository,
+  type UserRepository,
   UserNotFound
 } from '@/domain/user'
 
 export class UserLogin {
-  constructor(private readonly userRepository: IUserRepository) {}
+  constructor(private readonly userRepository: UserRepository) {}
 
   async login(props: UserPropsLoginInput): Promise<User> {
-    const user = await this.userRepository.findAndValidate(props.email, props.password)
+    const user = await this.userRepository.getByEmail(props.email)
     const isValidPass = await user.getProps().password.compare(props.password)
     if (!isValidPass) throw new UserNotFound(props.email, 'email')
     return user
