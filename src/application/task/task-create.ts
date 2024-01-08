0605cb720723fa275b6cacec7a5e4d4a834bb60c
Franco de Desarrollo <francoegi01@gmail.com>
2024-01-08 18:24:19 -0300

    feat(app): update create task adding pomodoro

--- src/application/task/task-create.ts
+++ src/application/task/task-create.ts
@@ -1,9 +1,14 @@
 import { valdiateID } from '@/domain/core'
 import { validateTask, type TaskPropsCreate, Task, type TaskRepository } from '@/domain/task'
+import { Pomodoro } from '@/domain/task/value-objects'
 import { type User } from '@/domain/user'
 
 interface CreateTaskProps {
-  task: Omit<TaskPropsCreate, 'userId'>
+  task: Omit<TaskPropsCreate, 'userId' | 'pomodoro'> & {
+    pomodoro: {
+      estimated: number
+    }
+  }
   userId: User['id']
 }
 export class CreateTask {
@@ -14,20 +19,35 @@ export class CreateTask {
   async create(props: CreateTaskProps | CreateTaskProps[]): Promise<Task | Task[]> {
     if (Array.isArray(props)) {
       const tasks = await Promise.all(
-        props.map(async (p) => {
-          const taskInp = await validateTask(p.task)
+        props.map(async ({ task: { pomodoro, ...task }, ...p }) => {
+          const taskInp = await validateTask(task)
           const userId = await valdiateID(p.userId, 'User ID')
-          return Task.create({ ...taskInp, userId })
+          return Task.create({
+            ...taskInp,
+            userId,
+            pomodoro: new Pomodoro({
+              estimated: pomodoro.estimated,
+              actual: 0
+            })
+          })
         })
       )
       await this.taskRepository.transaction(async () => await this.taskRepository.create(tasks))
       return tasks
     } else {
-      const taskInp = await validateTask(props.task)
-      const userId = await valdiateID(props.userId, 'User ID')
-      const task = Task.create({ ...taskInp, userId })
-      await this.taskRepository.transaction(async () => await this.taskRepository.create(task))
-      return task
+      const {
+        task: { pomodoro, ...task },
+        ...p
+      } = props
+      const taskInp = await validateTask(task)
+      const userId = await valdiateID(p.userId, 'User ID')
+      const newTask = Task.create({
+        ...taskInp,
+        userId,
+        pomodoro: new Pomodoro({ estimated: props.task.pomodoro.estimated, actual: 0 })
+      })
+      await this.taskRepository.transaction(async () => await this.taskRepository.create(newTask))
+      return newTask
     }
   }
 }
