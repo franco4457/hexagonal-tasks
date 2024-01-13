bb8d3a7eaec041dfe188f14d8e5364d7de400763
Franco de Desarrollo <francoegi01@gmail.com>
2024-01-13 13:44:35 -0300

    test(application): update adding project and labels in cases adn pass all Use TEST_ID  from utils

--- __test__/application/task.test.ts
+++ __test__/application/task.test.ts
@@ -1,9 +1,10 @@
+import { TEST_ID } from './../utils/constants'
 import { ListTasks, CreateTask } from '@/application/task'
-import { Task } from '@/domain/task'
+import { ValidationError } from '@/domain/core'
+import { Label, Project, Task } from '@/domain/task'
 import { InMemoryTaskRepository } from '@/infraestructure/repository/in-memory'
 import EventEmitter2 from 'eventemitter2'
 
-const testUUID = 'c2d7e0e0-4e0a-4b7a-8c7e-2a9a9b0a3b1a'
 const eventEmitter = new EventEmitter2()
 const taskConfig = {
   eventEmitter,
@@ -14,6 +15,7 @@ const baseTask = {
   title: 'title',
   description: 'description',
   order: 1,
+  labels: [],
   pomodoro: {
     estimated: 1
   }
@@ -27,14 +29,136 @@ describe('task', () => {
     const createTask = new CreateTask(taskRepository)
     const task = await createTask.create({
       task: baseTask,
-      userId: testUUID
+      userId: TEST_ID
     })
     expect(task).toBeInstanceOf(Task)
     const taskProps = task.getProps()
     expect(taskProps.title).toEqual('title')
     expect(taskProps.description).toEqual('description')
     expect(task.id).toBeTypeOf('string')
-    expect(taskProps.userId).toBe(testUUID)
+    expect(taskProps.userId).toBe(TEST_ID)
+  })
+  it.concurrent('Create task with project', async () => {
+    const taskRepository = new InMemoryTaskRepository({
+      ...taskConfig
+    })
+    const createTask = new CreateTask(taskRepository)
+    const task = await createTask.create({
+      task: { ...baseTask, project: { name: 'project' } },
+      userId: TEST_ID
+    })
+    const taskProps = task.getProps()
+    expect(taskProps.project).toBeInstanceOf(Project)
+    expect(taskProps.project?.value.name).toBe('project')
+  })
+  it.concurrent('Create task with labels', async () => {
+    const taskRepository = new InMemoryTaskRepository({
+      ...taskConfig
+    })
+    const createTask = new CreateTask(taskRepository)
+    const task = await createTask.create({
+      task: {
+        ...baseTask,
+        labels: [
+          { name: 'label' },
+          {
+            id: TEST_ID,
+            name: 'label2'
+          }
+        ]
+      },
+      userId: TEST_ID
+    })
+    const taskProps = task.getProps()
+    expect(taskProps.labels).toBeInstanceOf(Array)
+    expect(taskProps.labels).toHaveLength(2)
+    expect(taskProps.labels[0].value.name).toBe('label')
+    expect(taskProps.labels[0]).toBeInstanceOf(Label)
+    expect(taskProps.labels[0].value.id).toBeTypeOf('string')
+    expect(taskProps.labels[1].value.name).toBe('label2')
+    expect(taskProps.labels[1].value.id).toBe(TEST_ID)
+    expect(taskProps.labels[1]).toBeInstanceOf(Label)
+  })
+  it.concurrent('Create task with labels and project', async () => {
+    const taskRepository = new InMemoryTaskRepository({
+      ...taskConfig
+    })
+    const createTask = new CreateTask(taskRepository)
+    const task = await createTask.create({
+      task: { ...baseTask, labels: [{ name: 'label' }], project: { name: 'project' } },
+      userId: TEST_ID
+    })
+    expect(task).toBeInstanceOf(Task)
+    const taskProps = task.getProps()
+    expect(taskProps.title).toEqual('title')
+    expect(taskProps.description).toEqual('description')
+    expect(task.id).toBeTypeOf('string')
+    expect(taskProps.userId).toBe(TEST_ID)
+    expect(taskProps.labels).toBeInstanceOf(Array)
+    expect(taskProps.labels).toHaveLength(1)
+    expect(taskProps.labels[0].value.name).toBe('label')
+    expect(taskProps.project).toBeInstanceOf(Project)
+    expect(taskProps.project?.value.name).toBe('project')
+  })
+  it.concurrent('Create multiple task', async () => {
+    const taskRepository = new InMemoryTaskRepository(taskConfig)
+    const createTask = new CreateTask(taskRepository)
+    const tasks = await createTask.create(
+      Array.from({ length: 10 }, (_, i) => i).map((i) => ({
+        task: { ...baseTask, title: `test-title-${i}`, description: `test-desc-${i}` },
+        userId: TEST_ID.slice(0, -1) + i
+      }))
+    )
+    expect(tasks).toBeInstanceOf(Array)
+    expect(tasks).toHaveLength(10)
+    tasks.forEach((task, i) => {
+      expect(task).toBeInstanceOf(Task)
+      const taskProps = task.getProps()
+      expect(taskProps.title).toBe(`test-title-${i}`)
+      expect(taskProps.description).toBe(`test-desc-${i}`)
+      expect(taskProps.userId).toBe(TEST_ID.slice(0, -1) + i)
+      expect(task.id).toBeTypeOf('string')
+    })
+  })
+  it.concurrent('Create multiple task with project and labels', async () => {
+    const taskRepository = new InMemoryTaskRepository(taskConfig)
+    const createTask = new CreateTask(taskRepository)
+    const tasks = await createTask.create(
+      Array.from({ length: 3 }, (_, i) => i).map((i) => ({
+        task: {
+          ...baseTask,
+          title: `test-title-${i}`,
+          description: `test-desc-${i}`,
+          labels: [
+            { name: 'label' },
+            {
+              id: TEST_ID,
+              name: 'label2'
+            }
+          ],
+          project: { name: 'project' }
+        },
+        userId: TEST_ID.slice(0, -1) + i
+      }))
+    )
+    expect(tasks).toBeInstanceOf(Array)
+    expect(tasks).toHaveLength(3)
+    tasks.forEach((task, i) => {
+      expect(task).toBeInstanceOf(Task)
+      const taskProps = task.getProps()
+      expect(taskProps.title).toBe(`test-title-${i}`)
+      expect(taskProps.description).toBe(`test-desc-${i}`)
+      expect(taskProps.userId).toBe(TEST_ID.slice(0, -1) + i)
+      expect(task.id).toBeTypeOf('string')
+      expect(taskProps.labels[0].value.name).toBe('label')
+      expect(taskProps.labels[0]).toBeInstanceOf(Label)
+      expect(taskProps.labels[0].value.id).toBeTypeOf('string')
+      expect(taskProps.labels[1].value.name).toBe('label2')
+      expect(taskProps.labels[1].value.id).toBe(TEST_ID)
+      expect(taskProps.labels[1]).toBeInstanceOf(Label)
+      expect(taskProps.project).toBeInstanceOf(Project)
+      expect(taskProps.project?.value.name).toBe('project')
+    })
   })
   it.concurrent('On create task should be publish event', async () => {
     const taskRepository = new InMemoryTaskRepository(taskConfig)
@@ -47,7 +171,7 @@ describe('task', () => {
     })
     const task = await createTask.create({
       task: baseTask,
-      userId: testUUID
+      userId: TEST_ID
     })
     await task.publishEvents(eventEmitter, console)
     expect(task).toBeInstanceOf(Task)
@@ -61,7 +185,7 @@ describe('task', () => {
     await createTask.create(
       Array.from({ length: 10 }, (_, i) => i).map((i) => ({
         task: { ...baseTask, title: `test-title-${i}`, description: `test-desc-${i}` },
-        userId: testUUID.slice(0, -1) + i
+        userId: TEST_ID.slice(0, -1) + i
       }))
     )
     const listTasks = new ListTasks(taskRepository)
@@ -73,7 +197,7 @@ describe('task', () => {
       const taskProps = task.getProps()
       expect(taskProps.title).toBe(`test-title-${i}`)
       expect(taskProps.description).toBe(`test-desc-${i}`)
-      expect(taskProps.userId).toBe(testUUID.slice(0, -1) + i)
+      expect(taskProps.userId).toBe(TEST_ID.slice(0, -1) + i)
       expect(task.id).toBeTypeOf('string')
     })
   })
@@ -86,16 +210,16 @@ describe('task', () => {
       await createTask.create(
         Array.from({ length: 10 }, (_, i) => i).map((i) => ({
           task: { ...baseTask, title: `test-title-${i}`, description: `test-desc-${i}` },
-          userId: testUUID.slice(0, -1) + i
+          userId: TEST_ID.slice(0, -1) + i
         }))
       ),
       createTask.create({
         task: { ...baseTask, title: 'test-title-10', description: 'test-desc-10' },
-        userId: testUUID.slice(0, -1) + 0
+        userId: TEST_ID.slice(0, -1) + 0
       })
     ])
     const listTasks = new ListTasks(taskRepository)
-    const tasks = await listTasks.getByUserId(testUUID.slice(0, -1) + 0)
+    const tasks = await listTasks.getByUserId(TEST_ID.slice(0, -1) + 0)
     expect(tasks).toBeInstanceOf(Array)
     expect(tasks).toHaveLength(2)
     const [task, task2] = tasks
@@ -104,72 +228,89 @@ describe('task', () => {
     expect(task).toBeInstanceOf(Task)
     expect(taskP.title).toBe('test-title-0')
     expect(taskP.description).toBe('test-desc-0')
-    expect(taskP.userId).toBe(testUUID.slice(0, -1) + 0)
+    expect(taskP.userId).toBe(TEST_ID.slice(0, -1) + 0)
     expect(task.id).toBeTypeOf('string')
     expect(task2).toBeInstanceOf(Task)
     expect(taskP2.title).toBe('test-title-10')
     expect(taskP2.description).toBe('test-desc-10')
-    expect(taskP2.userId).toBe(testUUID.slice(0, -1) + 0)
+    expect(taskP2.userId).toBe(TEST_ID.slice(0, -1) + 0)
     expect(task2.id).toBeTypeOf('string')
   })
-  it.concurrent('should throw error when create task don`t send correct ', async () => {
-    const taskRepository = new InMemoryTaskRepository(taskConfig)
-    const createTask = new CreateTask(taskRepository)
-    await expect(
-      createTask.create({
-        // @ts-expect-error test
-        task: {},
-        userId: testUUID
-      })
-    ).rejects.toThrowError('["Title is required","Description is required","Order is required"]')
-  })
-  it('should throw error when create task don`t send correct types ', async () => {
-    const taskRepository = new InMemoryTaskRepository(taskConfig)
-    const createTask = new CreateTask(taskRepository)
-    await expect(
-      createTask.create({
-        // @ts-expect-error test
-        task: {
-          title: 0,
-          description: 0
-        },
-        userId: testUUID
-      })
-    ).rejects.toThrowError(
-      '["Expected string, received number","Expected string, received number","Order is required"]'
-    )
-  })
-  it('should throw error when create task don`t send correct lengths', async () => {
-    const taskRepository = new InMemoryTaskRepository(taskConfig)
-    const createTask = new CreateTask(taskRepository)
-    await expect(
-      createTask.create({
-        task: {
-          ...baseTask,
-          order: -1,
-          title: 't',
-          description: 'd'
-        },
-        userId: testUUID
-      })
-    ).rejects.toThrowError(
-      '["Title should be at least 3 characters","Description should be at least 3 characters","Order should be equals or greater than 0"]'
-    )
-  })
-  it('should throw error when create task don`t incorrect userId', async () => {
-    const taskRepository = new InMemoryTaskRepository(taskConfig)
-    const createTask = new CreateTask(taskRepository)
-    await expect(
-      createTask.create({
-        task: {
-          ...baseTask,
-          title: 'title',
-          description: 'description'
-        },
-        userId: 'asdjkah-asldjkh'
-      })
-    ).rejects.toThrowError(
-      '["User ID should be a valid UUID(something like this: \'string-string-string-string-string\')"]'
-    )
+
+  describe.concurrent('Exceptions', () => {
+    it.concurrent('Don`t send correct props', async () => {
+      const taskRepository = new InMemoryTaskRepository(taskConfig)
+      const createTask = new CreateTask(taskRepository)
+      await expect(
+        createTask.create({
+          // @ts-expect-error test
+          task: {},
+          userId: TEST_ID
+        })
+      ).rejects.toThrowError(
+        '["Title is required","Description is required","Order is required","Labels is required"]'
+      )
+    })
+    it.concurrent('Don`t send correct types ', async () => {
+      const taskRepository = new InMemoryTaskRepository(taskConfig)
+      const createTask = new CreateTask(taskRepository)
+      try {
+        await createTask.create({
+          // @ts-expect-error test
+          task: {
+            title: 0,
+            description: 0,
+            labels: 'foo',
+            order: 'bar',
+            project: {
+              name: {},
+              id: 'foo'
+            },
+            pomodoro: 'baz'
+          },
+          userId: TEST_ID
+        })
+        expect(true).toBe(false)
+      } catch (error) {
+        expect(error).toBeInstanceOf(ValidationError)
+        const e = error as ValidationError
+        expect(e.message).toBe(
+          '["Invalid type on On \'title\'. expected string, received number","Invalid type on On \'description\'. expected string, received number","Invalid type on On \'order\'. expected number, received string","Invalid project id","Invalid type on On \'project.name\'. expected string, received object","Invalid type on On \'labels\'. expected array, received string"]'
+        )
+      }
+    })
+    it.concurrent('Don`t send correct lengths', async () => {
+      const taskRepository = new InMemoryTaskRepository(taskConfig)
+      const createTask = new CreateTask(taskRepository)
+      await expect(
+        createTask.create({
+          task: {
+            ...baseTask,
+            order: -1,
+            title: 't',
+            description: 'd'
+          },
+          userId: TEST_ID
+        })
+      ).rejects.toThrowError(
+        '["Title should be at least 3 characters","Description should be at least 3 characters","Order should be equals or greater than 0"]'
+      )
+    })
+    it.concurrent('Don`t correct userId', async () => {
+      const taskRepository = new InMemoryTaskRepository(taskConfig)
+      const createTask = new CreateTask(taskRepository)
+      await expect(
+        createTask.create({
+          task: {
+            ...baseTask,
+            title: 'title',
+            description: 'description'
+          },
+          userId: 'asdjkah-asldjkh'
+        })
+      ).rejects.toThrowError(
+        '["User ID should be a valid UUID(something like this: \'string-string-string-string-string\')"]'
+      )
+    })
   })
 })
