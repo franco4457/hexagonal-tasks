2f296d52574beaaebee1ffd77bfbcc6c078ae35e
Franco de Desarrollo <francoegi01@gmail.com>
2024-01-13 13:45:23 -0300

    test(infra/express): update adding project and labels in cases and pass all Use TEST_ID  from utils

--- __test__/infra/express/task.test.ts
+++ __test__/infra/express/task.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest'
 import api from '@/infraestructure/express/app'
+import { TEST_ID } from '../../utils/constants'
 const app = (await api).getInstance()
 
 const testTask = {
@@ -9,7 +10,8 @@ const testTask = {
   pomodoro: {
     estimated: 1
   },
-  userId: 'c2d7e0e0-4e0a-4b7a-8c7e-2a9a9b0a3b1a'
+  labels: [],
+  userId: TEST_ID
 }
 
 describe('Task', () => {
@@ -18,10 +20,36 @@ describe('Task', () => {
     expect(res.status).toBe(201)
     const { task } = res.body
     expect(task).toHaveProperty('id')
-    expect(task).toHaveProperty('title')
+    expect(task.title).toBe(testTask.title)
     expect(task.isCompleted).toBe(false)
-    expect(task).toHaveProperty('description')
-    expect(task).toHaveProperty('userId')
+    expect(task.description).toBe('description')
+    expect(task.userId).toBe(TEST_ID)
+    expect(task.labels).toEqual([])
+    expect(task.project).toBe(null)
+  })
+  it.concurrent('POST /task - whit project and labels', async () => {
+    const res = await request(app)
+      .post('/api/v1/task')
+      .send({
+        ...testTask,
+        labels: [{ name: 'label' }, { id: TEST_ID, name: 'label2' }],
+        project: { name: 'project' }
+      })
+    expect(res.status).toBe(201)
+    const { task } = res.body
+    expect(task).toHaveProperty('id')
+    expect(task.title).toBe(testTask.title)
+    expect(task.isCompleted).toBe(false)
+    expect(task.description).toBe('description')
+    expect(task.userId).toBe(TEST_ID)
+    expect(task.labels).toEqual([
+      { id: expect.any(String), name: 'label' },
+      { id: TEST_ID, name: 'label2' }
+    ])
+    expect(task.project).toEqual({
+      id: expect.any(String),
+      name: 'project'
+    })
   })
   it.concurrent('POST /task - error missig fields', async () => {
     const res = await request(app).post('/api/v1/task').send({})
@@ -50,9 +78,21 @@ describe('Task', () => {
           received: 'undefined',
           path: ['order'],
           message: 'Order is required'
+        },
+        {
+          code: 'invalid_type',
+          expected: 'array',
+          received: 'undefined',
+          path: ['labels'],
+          message: 'Labels is required'
         }
       ],
-      errors: ['Title is required', 'Description is required', 'Order is required'],
+      errors: [
+        'Title is required',
+        'Description is required',
+        'Order is required',
+        'Labels is required'
+      ],
       message: 'Invalida Task'
     })
   })
@@ -63,6 +103,9 @@ describe('Task', () => {
         title: { title: 'title' },
         description: 20,
         order: 'one',
+        pomodoro: [],
+        labels: {},
+        project: { name: {} },
         userId: testTask.userId
       })
     expect(res.status).toBe(422)
@@ -75,38 +118,54 @@ describe('Task', () => {
           expected: 'string',
           received: 'object',
           path: ['title'],
-          message: 'Expected string, received object'
+          message: "Invalid type on On 'title'. expected string, received object"
         },
         {
           code: 'invalid_type',
           expected: 'string',
           received: 'number',
           path: ['description'],
-          message: 'Expected string, received number'
+          message: "Invalid type on On 'description'. expected string, received number"
         },
         {
           code: 'invalid_type',
           expected: 'number',
           received: 'string',
           path: ['order'],
-          message: 'Expected number, received string'
+          message: "Invalid type on On 'order'. expected number, received string"
+        },
+        {
+          code: 'invalid_type',
+          expected: 'string',
+          received: 'object',
+          path: ['project', 'name'],
+          message: "Invalid type on On 'project.name'. expected string, received object"
+        },
+        {
+          code: 'invalid_type',
+          expected: 'array',
+          received: 'object',
+          path: ['labels'],
+          message: "Invalid type on On 'labels'. expected array, received object"
         }
       ],
       errors: [
-        'Expected string, received object',
-        'Expected string, received number',
-        'Expected number, received string'
+        "Invalid type on On 'title'. expected string, received object",
+        "Invalid type on On 'description'. expected string, received number",
+        "Invalid type on On 'order'. expected number, received string",
+        "Invalid type on On 'project.name'. expected string, received object",
+        "Invalid type on On 'labels'. expected array, received object"
       ],
       message: 'Invalida Task'
     })
   })
   it.concurrent('POST /task - error invalid uuid', async () => {
-    const res = await request(app).post('/api/v1/task').send({
-      title: 'title',
-      description: 'description',
-      order: 1,
-      userId: '123'
-    })
+    const res = await request(app)
+      .post('/api/v1/task')
+      .send({
+        ...testTask,
+        userId: '123'
+      })
     expect(res.status).toBe(422)
     expect(res.body).toEqual({
       error: true,
@@ -144,10 +203,29 @@ describe('Task', () => {
     const res = await request(app).get('/api/v1/task')
     expect(res.status).toBe(200)
     const { tasks } = res.body
-    expect(tasks).toHaveLength(1)
-    expect(tasks[0]).toHaveProperty('id')
-    expect(tasks[0]).toHaveProperty('title')
-    expect(tasks[0]).toHaveProperty('description')
-    expect(tasks[0]).toHaveProperty('userId')
+    expect(tasks).toHaveLength(2)
+    const [task, task2] = tasks
+    expect(task).toHaveProperty('id')
+    expect(task.title).toBe(testTask.title)
+    expect(task.isCompleted).toBe(false)
+    expect(task.description).toBe('description')
+    expect(task.userId).toBe(TEST_ID)
+    expect(task.labels).toEqual([])
+    expect(task.project).toBe(null)
+
+    // second task
+    expect(task2).toHaveProperty('id')
+    expect(task2.title).toBe(testTask.title)
+    expect(task2.isCompleted).toBe(false)
+    expect(task2.description).toBe('description')
+    expect(task2.userId).toBe(TEST_ID)
+    expect(task2.labels).toEqual([
+      { id: expect.any(String), name: 'label' },
+      { id: TEST_ID, name: 'label2' }
+    ])
+    expect(task2.project).toEqual({
+      id: expect.any(String),
+      name: 'project'
+    })
   })
 })
