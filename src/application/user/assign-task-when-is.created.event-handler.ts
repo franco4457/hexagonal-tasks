9d114ef8aa3796bb7c86d054b717349c3131dd64
Franco de Desarrollo <francoegi01@gmail.com>
2024-01-14 14:25:26 -0300

    fix(app): add named props to event handler

--- src/application/user/assign-task-when-is.created.event-handler.ts
+++ src/application/user/assign-task-when-is.created.event-handler.ts
@@ -5,7 +5,10 @@ import { UserNotFound, type UserRepository } from '@/domain/user'
 export class AssignTaskWhenIsCreatedEventHandler implements EventHandler {
   static EVENT_NAME = TaskCreateEvent.name
   static repositoriesToInject: Repositories[] = ['userRepository' as Repositories]
-  constructor(protected readonly userRepository: UserRepository) {}
+  private readonly userRepository: UserRepository
+  constructor(props: { userRepository: UserRepository }) {
+    this.userRepository = props.userRepository
+  }
 
   async handle(event: TaskCreateEvent): Promise<void> {
     const user = await this.userRepository.getById(event.userId)
