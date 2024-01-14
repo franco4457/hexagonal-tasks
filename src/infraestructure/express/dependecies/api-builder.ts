c309bc0c55675eddfa0937e1a9d062bab118ed45
Franco de Desarrollo <francoegi01@gmail.com>
2024-01-14 14:24:26 -0300

    refactor(infra): add awilix for injection dependencies

--- src/infraestructure/express/dependecies/api-builder.ts
+++ src/infraestructure/express/dependecies/api-builder.ts
@@ -1,60 +1,83 @@
 import type { IApiBuilder } from '@/application/api/builder'
-import type { TaskRepository } from '@/domain/task'
-import { type UserRepository } from '@/domain/user'
+import { TaskRepository } from '@/domain/task'
+import { UserRepository } from '@/domain/user'
 import { ApiExpress } from '../api'
 import { MainRouter } from '../routes'
 import EventEmitter2 from 'eventemitter2'
 import { AssignTaskWhenIsCreatedEventHandler } from '@/application/user'
+import { asClass, asValue, createContainer } from 'awilix'
 
 const eventhandlers = [AssignTaskWhenIsCreatedEventHandler]
+
+class EventEmitter extends EventEmitter2 {
+  // eslint-disable-next-line @typescript-eslint/no-useless-constructor
+  constructor() {
+    super()
+  }
+}
+
 export class ApiBuilderExpress implements IApiBuilder {
   private api: ApiExpress
-  private taskRepository!: TaskRepository | null
-  private userRepository!: UserRepository | null
-  private readonly eventEmitter = new EventEmitter2()
   private readonly appContext = 'EXPRESS'
+  private readonly container = createContainer()
+
   constructor() {
-    this.taskRepository = null
-    this.userRepository = null
+    this.container.register({
+      eventEmitter: asClass(EventEmitter).singleton(),
+      appContext: asValue(this.appContext)
+    })
     this.api = new ApiExpress()
   }
 
   getRepoConfig(): { eventEmitter: EventEmitter2; appContext?: string } {
     return {
-      eventEmitter: this.eventEmitter,
+      eventEmitter: this.container.resolve('eventEmitter'),
       appContext: this.appContext
     }
   }
 
   reset(): IApiBuilder {
-    this.taskRepository = null
-    this.userRepository = null
     this.api = new ApiExpress()
     return this
   }
 
   setTaskRepository(taskRepository: TaskRepository): IApiBuilder {
-    this.taskRepository = taskRepository
+    this.container.register({
+      taskRepository: asValue(taskRepository)
+    })
     return this
   }
 
   setUserRepository(userRepository: UserRepository): IApiBuilder {
-    this.userRepository = userRepository
+    this.container.register({
+      userRepository: asValue(userRepository)
+    })
     return this
   }
 
   build(): ApiExpress {
-    if (this.taskRepository == null) throw new Error('TaskRepository not set')
-    if (this.userRepository == null) throw new Error('UserRepository not set')
+    if (!(this.container.resolve('taskRepository') instanceof TaskRepository)) {
+      throw new Error('TaskRepository not set')
+    }
+    if (!(this.container.resolve('userRepository') instanceof UserRepository)) {
+      throw new Error('UserRepository not set')
+    }
+
     eventhandlers.forEach((Handler) => {
-      const repositoriesToInject = Handler.repositoriesToInject.map((repo) => {
-        return this[repo]
-      })
-      const instance = new Handler(...(repositoriesToInject as [UserRepository])) // Convert repositoriesToInject to a tuple
-      this.eventEmitter.on(Handler.EVENT_NAME, instance.handle.bind(instance))
+      const instance = this.container
+        .register({
+          [Handler.name]: asClass(Handler).singleton()
+        })
+        .resolve(Handler.name)
+      this.container.resolve('eventEmitter').on(Handler.EVENT_NAME, instance.handle.bind(instance))
     })
 
-    this.api.build(new MainRouter(this.userRepository, this.taskRepository))
+    this.api.build(
+      new MainRouter(
+        this.container.resolve('userRepository'),
+        this.container.resolve('taskRepository')
+      )
+    )
     return this.api
   }
 }
