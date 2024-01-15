a6a5b37fdc12d150c5bc01150890a6db8a18add4
Franco de Desarrollo <francoegi01@gmail.com>
2024-01-15 20:34:35 -0300

    feat(domain): add custom errors to pomodoro rm todos

--- src/domain/task/value-objects/pomodoro.ts
+++ src/domain/task/value-objects/pomodoro.ts
@@ -1,25 +1,23 @@
-import { ValueObject } from '@/domain/core'
+import { ValidationError, ValueObject } from '@/domain/core'
 
 export interface PomodoroProps {
   estimated: number
   actual: number
 }
-// TODO: add events to pomodoro
 export class Pomodoro extends ValueObject<PomodoroProps> {
   static create(props: PomodoroProps): Pomodoro {
     return new Pomodoro(props)
   }
 
-  // TODO: add custom Errors
   protected validate(props: PomodoroProps): void {
     if (props.estimated < 0) {
-      throw new Error('Estimated should be greater than 0')
+      throw new ValidationError('Estimated should be greater than 0')
     }
     if (props.actual < 0) {
-      throw new Error('Actual should be greater than 0')
+      throw new ValidationError('Actual should be greater than 0')
     }
     if (props.actual > props.estimated) {
-      throw new Error('Actual should be less than estimated')
+      throw new ValidationError('Actual should be less than estimated')
     }
   }
 }
