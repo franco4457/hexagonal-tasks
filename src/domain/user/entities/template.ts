c9939601e7aa66032e426738b9fc063dacb2aa11
Franco de Desarrollo <francoegi01@gmail.com>
2024-01-17 21:25:08 -0300

    feat(domain): user template task VO and user template entity

---
+++ src/domain/user/entities/template.ts
@@ -0,0 +1,25 @@
+import { Entity, ValidationError } from '@/domain/core'
+import { randomUUID } from 'crypto'
+import { type TaskTemplate } from '../value-objects'
+
+export interface TemplateProps {
+  name: string
+  tasks: TaskTemplate[]
+}
+
+export class Template extends Entity<TemplateProps> {
+  public static create(props: TemplateProps): Template {
+    const id = randomUUID()
+    return new Template({ props, id })
+  }
+
+  validate(): void {
+    const { name, tasks } = this.props
+    if (name.length < 3) {
+      throw new ValidationError('Template name must be at least 3 characters long')
+    }
+    if (tasks.length < 1) {
+      throw new ValidationError('Template must have at least one task')
+    }
+  }
+}
---
+++ src/domain/user/value-objects/task-template.ts
@@ -0,0 +1,30 @@
+import { ValidationError, ValueObject } from '@/domain/core'
+
+interface TaskTemplateProps {
+  name: string
+  description: string
+  order: number
+  pomodoroEstimated: number
+  projectId: string | null
+}
+
+export class TaskTemplate extends ValueObject<TaskTemplateProps> {
+  protected validate(value: TaskTemplateProps): void {
+    const { name, description, order, pomodoroEstimated, projectId } = value
+    if (name.length < 3) {
+      throw new ValidationError('Template name must be at least 3 characters long')
+    }
+    if (description.length < 3) {
+      throw new ValidationError('Template description must be at least 3 characters long')
+    }
+    if (order < 0) {
+      throw new ValidationError('Template order must be greater than 0')
+    }
+    if (pomodoroEstimated < 0) {
+      throw new ValidationError('Template pomodoroEstimated must be greater than 0')
+    }
+    if (projectId != null && projectId.length < 3) {
+      throw new ValidationError('Template projectId must be at least 3 characters long')
+    }
+  }
+}
