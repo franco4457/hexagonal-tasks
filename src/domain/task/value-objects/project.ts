7f92d761cb5774714fdbf34d2389faf67d2d89cf
Franco de Desarrollo <francoegi01@gmail.com>
2024-01-19 19:01:01 -0300

    refactor(domain/task): remove id to project task

--- src/domain/task/value-objects/project.ts
+++ src/domain/task/value-objects/project.ts
@@ -1,23 +1,17 @@
 import { ArgumentNotProvided, ValueObject, isEmpty } from '@/domain/core'
-import { randomUUID } from 'crypto'
 
 interface ProjectProps {
   name: string
-  id: string
 }
 
 type ProjectPropsCreate = Omit<ProjectProps, 'id'>
 
 export class Project extends ValueObject<ProjectProps> {
   static create = ({ name }: ProjectPropsCreate): Project => {
-    const id = randomUUID()
-    return new Project({ id, name })
+    return new Project({ name })
   }
 
   public validate(value: ProjectProps): void {
-    if (isEmpty(value.id)) {
-      throw new ArgumentNotProvided('Project id cannot be empty')
-    }
     if (isEmpty(value.name)) {
       throw new ArgumentNotProvided('Project name cannot be empty')
     }
