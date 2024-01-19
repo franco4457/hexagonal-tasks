d7a57782f7b888aada5dfeb8f4fbf90276d7fb7f
Franco de Desarrollo <francoegi01@gmail.com>
2024-01-19 18:33:58 -0300

    feat(domain/project): add pomodoro count prop

--- src/domain/project/project.entity.ts
+++ src/domain/project/project.entity.ts
@@ -5,21 +5,24 @@ import { ProjectCreateEvent } from './event'
 export interface ProjectProps {
   name: string
   userId: string
+  pomodoroCount: number
 }
 
 // TODO: Fix extends Record<string, unknown> throw type error on ProjectRepository
 export interface ProjectModel extends ProjectProps, Record<string, unknown> {
   id: string
+  pomodoroCount: number
   name: string
   userId: string
   createdAt: Date
   updatedAt: Date
 }
 
+type CreateProjectProps = Omit<ProjectProps, 'pomodoroCount'>
 export class Project extends AggregateRoot<ProjectProps> {
-  public static create(props: ProjectProps): Project {
+  public static create(props: CreateProjectProps): Project {
     const id = randomUUID()
-    const project = new Project({ id, props })
+    const project = new Project({ id, props: { ...props, pomodoroCount: 0 } })
     project.addEvent(
       new ProjectCreateEvent({ aggregateId: id, name: props.name, userId: props.userId })
     )
@@ -27,12 +30,15 @@ export class Project extends AggregateRoot<ProjectProps> {
   }
 
   public validate(): void {
-    const { name, userId } = this.props
+    const { name, userId, pomodoroCount } = this.props
     if (isEmpty(name)) {
       throw new Error('Project name must be at least 3 characters long')
     }
     if (isEmpty(userId)) {
       throw new Error('Project userId must be at least 3 characters long')
+    }
+    if (pomodoroCount < 0) {
+      throw new Error('Project pomodoroCount must be greater than 0')
     }
   }
 }
