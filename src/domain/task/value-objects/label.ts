e23be26df572a120a25ea6a323dd88bafd9feaf7
Franco de Desarrollo <francoegi01@gmail.com>
2024-01-20 12:11:30 -0300

    refactor(domain/task): remove id into label VO

--- src/domain/task/value-objects/label.ts
+++ src/domain/task/value-objects/label.ts
@@ -1,23 +1,11 @@
 import { ArgumentNotProvided, ValueObject, isEmpty } from '@/domain/core'
-import { randomUUID } from 'crypto'
 
 interface LabelProps {
   name: string
-  id: string
 }
 
-type LabelPropsCreate = Omit<LabelProps, 'id'>
-
 export class Label extends ValueObject<LabelProps> {
-  static create = ({ name }: LabelPropsCreate): Label => {
-    const id = randomUUID()
-    return new Label({ id, name })
-  }
-
   public validate(value: LabelProps): void {
-    if (isEmpty(value.id)) {
-      throw new ArgumentNotProvided('Label id cannot be empty')
-    }
     if (isEmpty(value.name)) {
       throw new ArgumentNotProvided('Label name cannot be empty')
     }
