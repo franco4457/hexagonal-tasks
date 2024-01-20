f20f45172940f2ca96af5bf9262c9ed8c44575cd
Franco de Desarrollo <francoegi01@gmail.com>
2024-01-20 12:15:54 -0300

    feat(domain/user): add method and event to remove template

---
+++ src/domain/user/events/user-remove-template.ts
@@ -0,0 +1,11 @@
+import { DomainEvent, type DomainEventProps } from '@/domain/core'
+
+export class UserRemoveTemplateEvent extends DomainEvent {
+  templateId: string
+  templateName: string
+  constructor(props: DomainEventProps<UserRemoveTemplateEvent>) {
+    super(props)
+    this.templateId = props.templateId
+    this.templateName = props.templateName
+  }
+}
