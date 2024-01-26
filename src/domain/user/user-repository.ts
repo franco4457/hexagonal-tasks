d73d14aa1239869e7dc36568a9da071d180e57b5
Franco de Desarrollo <francoegi01@gmail.com>
2024-01-26 17:03:13 -0300

    feat(domain/user): mixins to handle template and label enities

--- src/domain/user/user-repository.ts
+++ src/domain/user/user-repository.ts
@@ -2,13 +2,22 @@ import type EventEmitter2 from 'eventemitter2'
 import { type LoggerPort, RepositoryBase } from '../core'
 import type { User, UserModel } from './user.entity'
 import { UserMapper } from './user.mapper'
-import { type Template } from './entities'
+import { type Label, type Template } from './entities'
 
 export interface IUserRepository {
   getById: (id: string) => Promise<User>
   getByEmail: (email: string) => Promise<User>
   getAll: () => Promise<User[]>
   create: (user: User) => Promise<User>
+
+  addTemplate: (props: { userId: string; template: Template }) => Promise<Template>
+  getTemplatesByUserId: (userId: string) => Promise<Template[]>
+  updateTemplate: (props: { template: Template }) => Promise<Template>
+  removeTemplate: (props: { templateId: Template['id'] }) => Promise<void>
+
+  addLabelToUser: (props: { userId: string; label: Label }) => Promise<Label>
+  getLabelsByUserId: (userId: string) => Promise<Label[]>
+  removeLabel: (props: { labelId: Label['id'] }) => Promise<void>
 }
 
 export abstract class UserRepository
@@ -25,5 +34,15 @@ export abstract class UserRepository
   abstract getByEmail(email: string): Promise<User>
   abstract getAll(): Promise<User[]>
   abstract create(user: User): Promise<User>
+
+  // Templates
+  abstract addTemplate(props: { userId: string; template: Template }): Promise<Template>
   abstract getTemplatesByUserId(userId: string): Promise<Template[]>
+  abstract updateTemplate(props: { template: Template }): Promise<Template>
+  abstract removeTemplate(props: { templateId: Template['id'] }): Promise<void>
+
+  // Labels
+  abstract addLabelToUser(props: { userId: string; label: Label }): Promise<Label>
+  abstract getLabelsByUserId(userId: string): Promise<Label[]>
+  abstract removeLabel(props: { labelId: Label['id'] }): Promise<void>
 }
