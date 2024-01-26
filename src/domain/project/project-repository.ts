d36a4823e0a84ebcebebdd09931345b35b7bb317
Franco de Desarrollo <francoegi01@gmail.com>
2024-01-26 16:13:14 -0300

    feat(domain/project): add method to find project by name and userId

--- src/domain/project/project-repository.ts
+++ src/domain/project/project-repository.ts
@@ -8,6 +8,7 @@ export interface IProjectRepository {
   delete: (id: Project['id']) => Promise<void>
   getById: (id: Project['id']) => Promise<Project>
   getByUserId: (userId: string) => Promise<Project[]>
+  getByNameAndUserId: (query: { name: string; userId: string }) => Promise<Project>
   sumPomodoroCount: (id: Project['id']) => Promise<void>
 }
 
@@ -23,6 +24,7 @@ export abstract class ProjectRepository
   abstract delete(id: Project['id']): Promise<void>
   abstract getById(id: Project['id']): Promise<Project>
   abstract getByUserId(userId: string): Promise<Project[]>
+  abstract getByNameAndUserId(query: { name: string; userId: string }): Promise<Project>
 
   abstract sumPomodoroCount(id: Project['id']): Promise<void>
 }
