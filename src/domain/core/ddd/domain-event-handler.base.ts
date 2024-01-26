a909e66a07573d96356af64409a0501072191a87
Franco de Desarrollo <francoegi01@gmail.com>
2024-01-26 16:10:41 -0300

    feat(domain/core): add repositories to event handler

--- src/domain/core/ddd/domain-event-handler.base.ts
+++ src/domain/core/ddd/domain-event-handler.base.ts
@@ -1,6 +1,10 @@
 import { type DomainEvent } from './domain-event.base'
 
-export type Repositories = 'userRepository' | 'taskRepository'
+export type Repositories =
+  | 'userRepository'
+  | 'taskRepository'
+  | 'projectRepository'
+  | 'timerRepository'
 
 export abstract class EventHandler {
   static EVENT_NAME: string
