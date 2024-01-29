dd18e2da37d8556399d02082135b294d26ac11bb
Franco de Desarrollo <francoegi01@gmail.com>
2024-01-29 20:01:01 -0300

    feat(domain/user): update template event

---
+++ src/domain/user/events/user-update-template.ts
@@ -0,0 +1,11 @@
+import { DomainEvent, type DomainEventProps } from '@/domain/core'
+
+export class UserUpdateTemplateEvent extends DomainEvent {
+  templateId: string
+  name: string
+  constructor(props: DomainEventProps<UserUpdateTemplateEvent>) {
+    super(props)
+    this.templateId = props.templateId
+    this.name = props.name
+  }
+}
