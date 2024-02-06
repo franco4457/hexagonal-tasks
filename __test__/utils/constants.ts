f145a61c123a1cf85e8c87b92ede1b80bdf640f5
Franco de Desarrollo <francoegi01@gmail.com>
2024-02-06 17:26:22 -0300

    chore: add repo config into test utils

--- __test__/utils/constants.ts
+++ __test__/utils/constants.ts
@@ -1,1 +1,7 @@
+import EventEmitter2 from 'eventemitter2'
+
 export const TEST_ID = 'c2d7e0e0-4e0a-4b7a-8c7e-2a9a9b0a3b1a'
+export const REPO_CONFIG = {
+  appContext: 'TEST',
+  eventEmitter: new EventEmitter2()
+}
