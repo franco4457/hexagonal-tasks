2d9a4597cd1eb8182c6e67f18845bdfbba9df814
Franco de Desarrollo <francoegi01@gmail.com>
2024-02-08 19:07:26 -0300

    feat(infra/express): set in-memory project and timer repository

--- src/infraestructure/express/dependecies/in-memory.ts
+++ src/infraestructure/express/dependecies/in-memory.ts
@@ -1,5 +1,7 @@
 import {
+  InMemoryProjectRepository,
   InMemoryTaskRepository,
+  InMemoryTimerRepository,
   InMemoryUserRepository
 } from '@/infraestructure/repository/in-memory'
 import { ApiBuilderExpress } from './api-builder'
@@ -10,7 +12,11 @@ export const createInMemoryApi = (): ApiExpress => {
   const config = apiBuilder.getRepoConfig()
   const userRepository = new InMemoryUserRepository(config)
   const taskRepository = new InMemoryTaskRepository(config)
+  const projectRepository = new InMemoryProjectRepository(config)
+  const timerRepository = new InMemoryTimerRepository(config)
   apiBuilder.setTaskRepository(taskRepository)
   apiBuilder.setUserRepository(userRepository)
+  apiBuilder.setProjectRepository(projectRepository)
+  apiBuilder.setTimerRepository(timerRepository)
   return apiBuilder.build()
 }
