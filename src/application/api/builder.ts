18512393c09a48481ca42dd8f0c8f52e180f0d22
Franco de Desarrollo <francoegi01@gmail.com>
2024-02-08 19:02:22 -0300

    feat(application/api): add project and timer repo setters

--- src/application/api/builder.ts
+++ src/application/api/builder.ts
@@ -1,4 +1,6 @@
+import { type ProjectRepository } from '@/domain/project'
 import { type TaskRepository } from '@/domain/task'
+import { type TimerRepository } from '@/domain/timer'
 import { type UserRepository } from '@/domain/user'
 import type EventEmitter2 from 'eventemitter2'
 
@@ -8,6 +10,8 @@ export interface IApiBuilder {
     eventEmitter: EventEmitter2
     appContext?: string
   }
+  setProjectRepository: (projectRepository: ProjectRepository) => IApiBuilder
+  setTimerRepository: (timerRepository: TimerRepository) => IApiBuilder
   setTaskRepository: (taskRepository: TaskRepository) => IApiBuilder
   setUserRepository: (userRepository: UserRepository) => IApiBuilder
   build: () => any
