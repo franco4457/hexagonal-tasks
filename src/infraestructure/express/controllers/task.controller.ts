61a3a0a08a3fdd2503e6ecfc5991058a512ca387
Franco de Desarrollo <francoegi01@gmail.com>
2024-02-10 13:17:28 -0300

    fix(infra/task): update controller after refactor

--- src/infraestructure/express/controllers/task.controller.ts
+++ src/infraestructure/express/controllers/task.controller.ts
@@ -1,23 +1,25 @@
 import { ListTasks } from '@/application/task/list-tasks'
-import { CreateTask } from '@/application/task/task-create'
+import { TaskCreateCommand, TaskCreateService } from '@/application/task'
 import { TaskMapper, type TaskRepository } from '@/domain/task'
 import { type NextFunction, type Request, type Response } from 'express'
 export class TaskController {
-  private readonly createTask: CreateTask
+  private readonly createTask: TaskCreateService
   private readonly listTasks: ListTasks
   private readonly mapper = new TaskMapper()
   constructor(private readonly taskRepository: TaskRepository) {
-    this.createTask = new CreateTask(this.taskRepository)
+    this.createTask = new TaskCreateService(this.taskRepository)
     this.listTasks = new ListTasks(this.taskRepository)
   }
 
   async create(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
       const { userId, title, description, order, pomodoro, labels, project } = req.body
-      const task = await this.createTask.create({
-        task: { title, description, order, pomodoro, labels, project },
-        userId
-      })
+      const task = await this.createTask.execute(
+        new TaskCreateCommand({
+          task: { title, description, order, pomodoro, labels, project },
+          userId
+        })
+      )
       res.status(201).json({ task: this.mapper.toResponse(task) })
     } catch (error) {
       next(error)
@@ -27,7 +29,7 @@ export class TaskController {
   async bulkCreate(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
       const { data } = req.body as { data: [] }
-      const tasksCreated = await this.createTask.create(data)
+      const tasksCreated = await this.createTask.execute(data)
       res.status(201).json({ tasks: tasksCreated.map((task) => this.mapper.toResponse(task)) })
     } catch (error) {
       next(error)
