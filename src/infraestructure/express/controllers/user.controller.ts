186cd05ac0e370fc958ea40d78980022b20e670c
Franco de Desarrollo <francoegi01@gmail.com>
2024-02-10 13:17:36 -0300

    fix(infra/user): update controller after refactor

--- src/infraestructure/express/controllers/user.controller.ts
+++ src/infraestructure/express/controllers/user.controller.ts
@@ -1,20 +1,20 @@
-import { UserLogin, UserRegister } from '@/application/user'
+import { UserLoginService, UserRegisterService } from '@/application/user'
 import { UserMapper, type UserRepository } from '@/domain/user'
 import type { Request, Response, NextFunction } from 'express'
 
 export class UserController {
-  private readonly userRegister: UserRegister
-  private readonly userLogin: UserLogin
+  private readonly userRegister: UserRegisterService
+  private readonly userLogin: UserLoginService
   private readonly mapper = new UserMapper()
   constructor(private readonly userRepostory: UserRepository) {
-    this.userRegister = new UserRegister(this.userRepostory)
-    this.userLogin = new UserLogin(this.userRepostory)
+    this.userRegister = new UserRegisterService(this.userRepostory)
+    this.userLogin = new UserLoginService(this.userRepostory)
   }
 
   async register(req: Request, res: Response, next: NextFunction): Promise<void> {
     const body = req.body
     try {
-      const user = await this.userRegister.register(body)
+      const user = await this.userRegister.execute(body)
       res.status(200).json({ user: this.mapper.toResponse(user) })
     } catch (error) {
       next(error)
@@ -24,7 +24,7 @@ export class UserController {
   async login(req: Request, res: Response, next: NextFunction): Promise<void> {
     const body = req.body
     try {
-      const user = await this.userLogin.login(body)
+      const user = await this.userLogin.execute(body)
       res.status(200).json({ user: this.mapper.toResponse(user) })
     } catch (error) {
       next(error)
