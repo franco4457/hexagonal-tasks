10795cf95ff82506af5d79335b1930255f6588b6
Franco de Desarrollo <francoegi01@gmail.com>
2024-02-11 13:57:42 -0300

    feat(domain/task): implement  query config, and add sorted by

--- src/domain/task/task-repository.ts
+++ src/domain/task/task-repository.ts
@@ -1,32 +1,57 @@
 import type EventEmitter2 from 'eventemitter2'
-import { type LoggerPort, RepositoryBase } from '../core'
-import type { User, IUserRepository } from '../user'
+import { type LoggerPort, RepositoryBase, type RepositoryQueryConfig } from '../core'
+import type { User } from '../user'
 import { type Task, type TaskModel } from './task.entity'
 import { TaskMapper } from './task.mapper'
 
+interface SortOptions extends RepositoryQueryConfig {
+  order: 'ASC' | 'DESC'
+  sortBy: keyof TaskModel
+}
 export interface ITaskRepository {
-  create: ((task: Task) => Promise<Task>) & ((task: Task[]) => Promise<Task[]>)
+  // TODO: Separate create method into two methods or only acepts array of tasks
+  create: (task: Task | Task[]) => Promise<Task | Task[]>
   getTask: (id: Task['id']) => Promise<Task>
   getTasks: () => Promise<Task[]>
-  getTasksByUserId: (userId: User['id']) => Promise<Task[]>
+
+  getTasksByUserId: (
+    userId: User['id'],
+    config?: RepositoryQueryConfig
+  ) => Promise<Task[] | TaskModel[]>
+
+  getTasksByUserIdSortedBy: (
+    userId: User['id'],
+    config?: SortOptions
+  ) => Promise<Task[] | TaskModel[]>
 
   updateLabels: (props: { task: Task }) => Promise<void>
 
   updateProject: (props: { task: Task }) => Promise<void>
 }
+
 export abstract class TaskRepository
   extends RepositoryBase<Task, TaskModel>
   implements ITaskRepository
 {
   readonly repositoryName = 'TaskRepository'
   protected readonly mapper = new TaskMapper()
-  protected aggregates: { userRepo?: IUserRepository } = {}
   constructor({ ...props }: { logger: LoggerPort; eventEmitter: EventEmitter2 }) {
     super({ ...props, mapper: new TaskMapper() })
   }
   abstract getTasks(): Promise<Task[]>
-  abstract getTasksByUserId(userId: User['id']): Promise<Task[]>
   abstract getTask(id: Task['id']): Promise<Task>
+
+  abstract getTasksByUserId(userId: User['id'], config: { raw: true }): Promise<TaskModel[]>
+  abstract getTasksByUserId(userId: User['id'], config?: RepositoryQueryConfig): Promise<Task[]>
+
+  abstract getTasksByUserIdSortedBy: ((
+    userId: User['id'],
+    config: Omit<SortOptions, 'raw'> & {
+      raw: true
+    }
+  ) => Promise<TaskModel[]>) &
+    ((userId: User['id'], config?: SortOptions) => Promise<Task[]>)
+
   abstract create(task: Task): Promise<Task>
   abstract create(task: Task[]): Promise<Task[]>
   abstract create(task: Task | Task[]): Promise<Task | Task[]>
