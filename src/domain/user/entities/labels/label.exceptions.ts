da4d51d8381a4c8e35727accd35437f140ddb6ae
Franco de Desarrollo <francoegi01@gmail.com>
2024-02-18 15:49:29 -0300

    feat(domain/user): add label exception

---
+++ src/domain/user/entities/labels/label.exceptions.ts
@@ -0,0 +1,8 @@
+import { ValidationError } from '@/domain/core'
+import { type ZodIssue } from 'zod'
+
+export class InvalidLabel extends ValidationError {
+  constructor(issues?: ZodIssue[]) {
+    super('Invalid label', issues)
+  }
+}
