7677df1a3dd2ea2237aa2460f0098926ee6a637f
Franco de Desarrollo <francoegi01@gmail.com>
2024-02-18 13:20:19 -0300

    fix(application/user): query name

--- src/application/user/queries/label-by-user-id.ts
+++ src/application/user/queries/label-by-user-id.ts
@@ -1,7 +1,7 @@
 import { type IQueryHandler, QueryBase } from '@/domain/core'
 import { type LabelModel, type UserRepository } from '@/domain/user'
 
-export class LabelByUserId extends QueryBase {
+export class LabelByUserIdQuery extends QueryBase {
   readonly userId: string
   constructor(props: { userId: string }) {
     super()
@@ -9,9 +9,9 @@ export class LabelByUserId extends QueryBase {
   }
 }
 
-export class LabelByUserIdHandler implements IQueryHandler<LabelByUserId, LabelModel[]> {
+export class LabelByUserIdHandler implements IQueryHandler<LabelByUserIdQuery, LabelModel[]> {
   constructor(private readonly userRepository: UserRepository) {}
-  async execute(query: LabelByUserId): Promise<LabelModel[]> {
+  async execute(query: LabelByUserIdQuery): Promise<LabelModel[]> {
     const { userId } = query
     return await this.userRepository.getLabelsByUserId(userId, { raw: true })
   }
