36cdad8499d30698a0691dc011da5f6ca068a40f
Franco de Desarrollo <francoegi01@gmail.com>
2024-02-19 20:57:12 -0300

    test(infra/user): add test for template and label

--- __test__/infra/express/user.test.ts
+++ __test__/infra/express/user.test.ts
@@ -1,7 +1,7 @@
 import request from 'supertest'
 
 import api from '@/infraestructure/express/app'
-const app = (await api).getInstance()
+import { TEST_ID } from '../../utils'
 const testUser = {
   email: 'test@email.com',
   name: 'test1',
@@ -12,9 +12,25 @@ const testLogin = {
   email: 'example@mail.com',
   password: 'Pass1234'
 }
-describe('User', () => {
-  it.concurrent('GET /user', async () => {
-    const res = await request(app).get('/api/v1/user')
+
+const testTemplate = {
+  name: 'test',
+  tasks: [
+    {
+      name: 'test',
+      description: 'test',
+      order: 1,
+      pomodoroEstimated: 1,
+      projectId: null
+    }
+  ]
+}
+// TODO: Check all test don't depend on each other
+describe.concurrent('User', async () => {
+  const app = (await api).getInstance()
+  it.concurrent('GET /user/all', async () => {
+    const res = await request(app).get('/api/v1/user/all')
+
     expect(res.status).toBe(200)
     expect(res.body.users.length).toBeGreaterThan(0)
     expect(res.body.users[0]).toHaveProperty('id')
@@ -25,7 +41,10 @@ describe('User', () => {
     expect(res.body.users[0]).not.toHaveProperty('password')
   })
   it.concurrent('POST /user/login', async () => {
-    const res = await request(app).post('/api/v1/user/login').send(testLogin)
+    const {
+      body: { token }
+    } = await request(app).post('/api/v1/user/login').send(testLogin)
+    const res = await request(app).get('/api/v1/user').set('Authorization', `Bearer ${token}`)
     expect(res.status).toBe(200)
     const { user } = res.body
     expect(user).toHaveProperty('id')
@@ -61,9 +80,13 @@ describe('User', () => {
     expect((await request(app).post('/api/v1/user/login').send(testLogin)).status).toBe(200)
   })
   it.concurrent('POST /user/register', async () => {
-    const res = await request(app)
+    const {
+      body: { token }
+    } = await request(app)
       .post('/api/v1/user/register')
       .send({ password: 'Pass1234', ...testUser })
+
+    const res = await request(app).get('/api/v1/user').set('Authorization', `Bearer ${token}`)
     expect(res.status).toBe(200)
     const {
       user: { id, createdAt, updatedAt, ...user }
@@ -197,6 +220,380 @@ describe('User', () => {
       error: true,
       name: 'Already exist',
       message: 'User with email: test2@email.com already exist'
+    })
+  })
+
+  describe.concurrent('User/Label', async () => {
+    let token = ''
+    beforeAll(async () => {
+      const {
+        body: { token: t }
+      } = await request(app).post('/api/v1/user/login').send(testLogin)
+      token = t
+    })
+    it.concurrent('GET /user/label', async () => {
+      const res = await request(app)
+        .get('/api/v1/user/label')
+        .set('Authorization', 'Bearer ' + token)
+      expect(res.status).toBe(200)
+      expect(res.body).toEqual({ labels: [] })
+    })
+    it.concurrent('POST /user/label', async () => {
+      const res = await request(app)
+        .post('/api/v1/user/label')
+        .send({ name: 'test' })
+        .set('Authorization', 'Bearer ' + token)
+      expect(res.status).toBe(201)
+      expect(res.body).toEqual({
+        label: {
+          id: expect.any(String),
+          name: 'test',
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String)
+        }
+      })
+      const res2 = await request(app)
+        .get('/api/v1/user/label')
+        .set('Authorization', 'Bearer ' + token)
+      expect(res2.status).toBe(200)
+      expect(res2.body).toEqual({
+        labels: [
+          {
+            id: expect.any(String),
+            name: 'test',
+            createdAt: expect.any(String),
+            updatedAt: expect.any(String)
+          }
+        ]
+      })
+    })
+
+    // XXX: this test depends on the previous one
+    it.concurrent('DELETE /user/label/:id', async () => {
+      const res1 = await request(app)
+        .get('/api/v1/user/label')
+        .set('Authorization', 'Bearer ' + token)
+      const labelId = res1.body.labels[0].id
+      const res = await request(app)
+        .delete(`/api/v1/user/label/${labelId}`)
+        .set('Authorization', 'Bearer ' + token)
+      expect(res.status).toBe(201)
+
+      const res2 = await request(app)
+        .get('/api/v1/user/label')
+        .set('Authorization', 'Bearer ' + token)
+      expect(res2.body).toEqual({ labels: [] })
+    })
+    it.concurrent('POST /user/label - error missing fields', async () => {
+      const res = await request(app)
+        .post('/api/v1/user/label')
+        .send({})
+        .set('Authorization', 'Bearer ' + token)
+      // expect(res.status).toBe(422)
+      expect(res.body).toEqual({
+        error: true,
+        name: 'Invalid label',
+        issues: [
+          {
+            code: 'invalid_type',
+            expected: 'string',
+            received: 'undefined',
+            path: ['name'],
+            message: 'Name is required'
+          }
+        ],
+        errors: ['Name is required'],
+        message: 'Invalid label'
+      })
+    })
+  })
+  describe.concurrent('User/Template', async () => {
+    // Token used for all test
+    let token = ''
+    beforeAll(async () => {
+      const {
+        body: { token: t }
+      } = await request(app).post('/api/v1/user/login').send(testLogin)
+      token = t
+    })
+    it.concurrent('GET /user/template', async () => {
+      const res = await request(app)
+        .get('/api/v1/user/template')
+        .set('Authorization', 'Bearer ' + token)
+      expect(res.status).toBe(200)
+      expect(res.body).toEqual({ templates: [] })
+    })
+    it.concurrent('POST /user/template', async () => {
+      const res = await request(app)
+        .post('/api/v1/user/template')
+        .send(testTemplate)
+        .set('Authorization', 'Bearer ' + token)
+      expect(res.status).toBe(201)
+      expect(res.body).toEqual({
+        template: {
+          id: expect.any(String),
+          name: 'test',
+          tasks: [
+            {
+              name: 'test',
+              description: 'test',
+              order: 1,
+              pomodoroEstimated: 1,
+              projectId: null
+            }
+          ],
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String)
+        }
+      })
+    })
+    it('PUT /user/template', async () => {
+      const res1 = await request(app)
+        .get('/api/v1/user/template')
+        .set('Authorization', 'Bearer ' + token)
+      const templateId = res1.body.templates[0].id
+      const res = await request(app)
+        .put('/api/v1/user/template')
+        .send({
+          name: 'updated-template',
+          id: templateId,
+          tasks: [
+            {
+              name: 'updated-template-taskName',
+              description: 'updated-template-taskDescription',
+              order: 2,
+              pomodoroEstimated: 3,
+              projectId: TEST_ID
+            }
+          ]
+        })
+        .set('Authorization', 'Bearer ' + token)
+      expect(res.status).toBe(201)
+
+      const res2 = await request(app)
+        .get('/api/v1/user/template')
+        .set('Authorization', 'Bearer ' + token)
+      const template = res2.body.templates.find((t: any) => t.id === templateId)
+      expect(template).toEqual({
+        id: templateId,
+        name: 'updated-template',
+        tasks: [
+          {
+            name: 'updated-template-taskName',
+            description: 'updated-template-taskDescription',
+            order: 2,
+            pomodoroEstimated: 3,
+            projectId: TEST_ID
+          }
+        ],
+        createdAt: expect.any(String),
+        updatedAt: expect.any(String)
+      })
+    })
+    it('DELETE /user/template/:id', async () => {
+      const res1 = await request(app)
+        .post('/api/v1/user/template')
+        .send(testTemplate)
+        .set('Authorization', 'Bearer ' + token)
+      const templateId = res1.body.template.id
+
+      const res = await request(app)
+        .delete(`/api/v1/user/template/${templateId}`)
+        .set('Authorization', 'Bearer ' + token)
+      expect(res.status).toBe(201)
+
+      const res2 = await request(app)
+        .get('/api/v1/user/template')
+        .set('Authorization', 'Bearer ' + token)
+      expect(res2.status).toBe(200)
+      expect(res2.body.templates.length).toBeGreaterThanOrEqual(0)
+      const template = res2.body.templates.find((t: any) => t.id === templateId)
+      expect(template).toBeUndefined()
+    })
+    it.concurrent('POST /user/template - error missing fields', async () => {
+      const res = await request(app)
+        .post('/api/v1/user/template')
+        .send({})
+        .set('Authorization', 'Bearer ' + token)
+      expect(res.status).toBe(422)
+      expect(res.body).toEqual({
+        error: true,
+        name: 'Invalid template',
+        issues: [
+          {
+            code: 'invalid_type',
+            expected: 'string',
+            received: 'undefined',
+            path: ['name'],
+            message: 'Name is required'
+          },
+          {
+            code: 'invalid_type',
+            expected: 'array',
+            received: 'undefined',
+            path: ['tasks'],
+            message: 'Tasks are required'
+          }
+        ],
+        errors: ['Name is required', 'Tasks are required'],
+        message: 'Invalid template'
+      })
+    })
+    it.concurrent('POST /user/template - error invalid types', async () => {
+      const res = await request(app)
+        .post('/api/v1/user/template')
+        .send({ name: 1, tasks: 1 })
+        .set('Authorization', 'Bearer ' + token)
+      expect(res.status).toBe(422)
+      expect(res.body).toEqual({
+        error: true,
+        name: 'Invalid template',
+        issues: [
+          {
+            code: 'invalid_type',
+            expected: 'string',
+            received: 'number',
+            path: ['name'],
+            message: "Invalid type on 'name'. expected string, received number"
+          },
+          {
+            code: 'invalid_type',
+            expected: 'array',
+            received: 'number',
+            path: ['tasks'],
+            message: "Invalid type on 'tasks'. expected array, received number"
+          }
+        ],
+        errors: [
+          "Invalid type on 'name'. expected string, received number",
+          "Invalid type on 'tasks'. expected array, received number"
+        ],
+        message: 'Invalid template'
+      })
+    })
+    it.concurrent('POST /user/template - error invalid tasks requireds', async () => {
+      const res = await request(app)
+        .post('/api/v1/user/template')
+        .send({ name: 'test', tasks: [{}] })
+        .set('Authorization', 'Bearer ' + token)
+      expect(res.status).toBe(422)
+      expect(res.body).toEqual({
+        error: true,
+        name: 'Invalid template',
+        issues: [
+          {
+            code: 'invalid_type',
+            expected: 'string',
+            received: 'undefined',
+            path: ['tasks', 0, 'name'],
+            message: 'Name is required'
+          },
+          {
+            code: 'invalid_type',
+            expected: 'string',
+            received: 'undefined',
+            path: ['tasks', 0, 'description'],
+            message: 'Description is required'
+          },
+          {
+            code: 'invalid_type',
+            expected: 'number',
+            received: 'undefined',
+            path: ['tasks', 0, 'order'],
+            message: 'Order is required'
+          },
+          {
+            code: 'invalid_type',
+            expected: 'number',
+            received: 'undefined',
+            path: ['tasks', 0, 'pomodoroEstimated'],
+            message: 'Pomodoro estimated is required'
+          },
+          {
+            code: 'invalid_type',
+            expected: 'string',
+            received: 'undefined',
+            path: ['tasks', 0, 'projectId'],
+            message: 'Project id is required'
+          }
+        ],
+        errors: [
+          'Name is required',
+          'Description is required',
+          'Order is required',
+          'Pomodoro estimated is required',
+          'Project id is required'
+        ],
+        message: 'Invalid template'
+      })
+    })
+    it.concurrent('POST /user/template - error invalid tasks types', async () => {
+      const res = await request(app)
+        .post('/api/v1/user/template')
+        .send({
+          name: 'test',
+          tasks: [
+            {
+              name: 1,
+              description: [],
+              order: {},
+              pomodoroEstimated: '1',
+              projectId: {}
+            }
+          ]
+        })
+        .set('Authorization', 'Bearer ' + token)
+      expect(res.status).toBe(422)
+      expect(res.body).toEqual({
+        error: true,
+        name: 'Invalid template',
+        issues: [
+          {
+            code: 'invalid_type',
+            expected: 'string',
+            received: 'number',
+            path: ['tasks', 0, 'name'],
+            message: "Invalid type on 'tasks[0].name'. expected string, received number"
+          },
+          {
+            code: 'invalid_type',
+            expected: 'string',
+            received: 'array',
+            path: ['tasks', 0, 'description'],
+            message: "Invalid type on 'tasks[0].description'. expected string, received array"
+          },
+          {
+            code: 'invalid_type',
+            expected: 'number',
+            received: 'object',
+            path: ['tasks', 0, 'order'],
+            message: "Invalid type on 'tasks[0].order'. expected number, received object"
+          },
+          {
+            code: 'invalid_type',
+            expected: 'number',
+            received: 'string',
+            path: ['tasks', 0, 'pomodoroEstimated'],
+            message:
+              "Invalid type on 'tasks[0].pomodoroEstimated'. expected number, received string"
+          },
+          {
+            code: 'invalid_type',
+            expected: 'string',
+            received: 'object',
+            path: ['tasks', 0, 'projectId'],
+            message: "Invalid type on 'tasks[0].projectId'. expected string, received object"
+          }
+        ],
+        errors: [
+          "Invalid type on 'tasks[0].name'. expected string, received number",
+          "Invalid type on 'tasks[0].description'. expected string, received array",
+          "Invalid type on 'tasks[0].order'. expected number, received object",
+          "Invalid type on 'tasks[0].pomodoroEstimated'. expected number, received string",
+          "Invalid type on 'tasks[0].projectId'. expected string, received object"
+        ],
+        message: 'Invalid template'
+      })
     })
   })
 })
