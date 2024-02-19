3f776ebd9264d381178b8882c144a70ea81003f3
Franco de Desarrollo <francoegi01@gmail.com>
2024-02-19 19:53:12 -0300

    feat(domain/user): add template exceptions and schema

---
+++ src/domain/user/entities/template/template.exceptions.ts
@@ -0,0 +1,8 @@
+import { ValidationError } from '@/domain/core'
+import type { ZodIssue } from 'zod'
+
+export class InvalidTemplate extends ValidationError {
+  constructor(issues?: ZodIssue[]) {
+    super('Invalid template', issues)
+  }
+}
