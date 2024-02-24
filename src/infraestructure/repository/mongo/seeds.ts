ad804884875558446333a5675ec9e497753213fd
Franco de Desarrollo <francoegi01@gmail.com>
2024-02-24 15:28:13 -0300

    fix(infra): clean task document on seeds

--- src/infraestructure/repository/mongo/seeds.ts
+++ src/infraestructure/repository/mongo/seeds.ts
@@ -13,9 +13,7 @@ export const testMongoSeeds = async (): Promise<void> => {
     await UserModel.deleteMany({
       id: 'c2d7e0e0-4e0a-4b7a-8c7e-2a9a9b0a3b1a'
     })
-    await TaskMongoModel.deleteMany({
-      title: 'title'
-    })
+    await TaskMongoModel.deleteMany({})
     await TimerMongoModel.deleteMany({})
     await LabelMongoModel.deleteMany({})
     await TemplateMongoModel.deleteMany({})
