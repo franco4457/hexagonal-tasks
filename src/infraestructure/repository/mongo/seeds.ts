f7492abae27f0a31a9844a974ccb790ef96af6a6
Franco de Desarrollo <francoegi01@gmail.com>
2024-02-24 14:59:05 -0300

    feat(infra): clean db and add missing fields in mongo seeds

--- src/infraestructure/repository/mongo/seeds.ts
+++ src/infraestructure/repository/mongo/seeds.ts
@@ -1,14 +1,25 @@
 import { conn } from './connect'
-import { TaskModel } from './task/model'
+import { ProjectMongoModel } from './project'
+import { TaskMongoModel } from './task/model'
+import { TimerMongoModel } from './timer'
+import { LabelMongoModel } from './user/label'
 import { UserModel } from './user/model'
+import { TemplateMongoModel } from './user/template'
 
 export const testMongoSeeds = async (): Promise<void> => {
   try {
     await conn()
     await UserModel.deleteMany({})
-    await TaskModel.deleteMany({
+    await UserModel.deleteMany({
+      id: 'c2d7e0e0-4e0a-4b7a-8c7e-2a9a9b0a3b1a'
+    })
+    await TaskMongoModel.deleteMany({
       title: 'title'
     })
+    await TimerMongoModel.deleteMany({})
+    await LabelMongoModel.deleteMany({})
+    await TemplateMongoModel.deleteMany({})
+    await ProjectMongoModel.deleteMany({})
     await Promise.all([
       UserModel.create({
         id: 'c2d7e0e0-4e0a-4b7a-8c7e-2a9a9b0a3b1a',
@@ -16,8 +27,12 @@ export const testMongoSeeds = async (): Promise<void> => {
         email: 'example@mail.com',
         lastname: 'tester',
         name: 'test',
-        password: 'Pass1234',
-        username: 'tested'
+        password: '$2b$10$LW29SqaXA.1e/ruyZjyjNumC7cItPePd2guY9Eq3udPl62iep9l6u',
+        username: 'tested',
+        labels: [],
+        templates: [],
+        createdAt: new Date(),
+        updatedAt: new Date()
       })
     ])
     console.log('Seeds created')
