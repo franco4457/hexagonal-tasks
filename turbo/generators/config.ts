4645cbaac84f6c9aa406f7498d3adfac0a321eca
Franco de Desarrollo <francoegi01@gmail.com>
2024-03-15 21:35:52 -0300

    feat(root): add turbo gen and template to genetare spaces

---
+++ turbo/generators/config.ts
@@ -0,0 +1,59 @@
+import { PlopTypes } from '@turbo/gen'
+
+const generators = ['domain', 'application', 'infrastructure']
+
+export default function generator(plop: PlopTypes.NodePlopAPI): void {
+  generators.forEach((gen) => {
+    plop.setGenerator(gen, {
+      description: `Generate a ${gen}`,
+      prompts: [
+        {
+          type: 'input',
+          name: `${gen}Name`,
+          message: `Enter ${gen} name`,
+          validate: (input: string) => {
+            if (input.includes('.')) {
+              return `${gen} name cannot include an extension`
+            }
+            if (input.includes(' ')) {
+              return `${gen} name cannot include spaces`
+            }
+            if (!input) {
+              return `${gen} name is required`
+            }
+            return true
+          }
+        },
+        {
+          type: 'input',
+          name: 'description',
+          message: `The description of this ${gen}:`
+        }
+      ],
+      actions(answers) {
+        const actions: PlopTypes.ActionType[] = []
+        if (!answers) return actions
+
+        const { description } = answers
+        const generatorName = answers[`${gen}Name`] ?? ''
+
+        const data = {
+          [`${gen}Name`]: generatorName,
+          description,
+          outDir: gen
+        }
+        console.log({ ...answers, data })
+        actions.push({
+          type: 'addMany',
+          destination: `{{ turbo.paths.root }}/packages/{{outDir}}/{{dashCase ${gen}Name}}`,
+          templateFiles: `{{ turbo.paths.root }}/turbo/generators/templates/${gen}/**`,
+          base: `{{ turbo.paths.root }}/turbo/generators/templates/${gen}`,
+          data,
+          abortOnFail: true
+        })
+        console.log({ actions })
+        return actions
+      }
+    })
+  })
+}
