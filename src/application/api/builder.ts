1f54437ea381997ad2e0a954e2ded35a3b1fe585
Franco de Desarrollo <francoegi01@gmail.com>
2024-03-16 14:40:37 -0300

    refactor(application/api): remove files to main root and implement package
    - eventually export all files on old index

--- src/application/api/builder.ts
+++ src/application/api/builder.ts
@@ -1,18 +1,1 @@
-import { type ProjectRepository } from '@/domain/project'
-import { type TaskRepository } from '@/domain/task'
-import { type TimerRepository } from '@/domain/timer'
-import { type UserRepository } from '@/domain/user'
-import type EventEmitter2 from 'eventemitter2'
-
-export interface IApiBuilder {
-  reset: () => IApiBuilder
-  getRepoConfig: () => {
-    eventEmitter: EventEmitter2
-    appContext?: string
-  }
-  setProjectRepository: (projectRepository: ProjectRepository) => IApiBuilder
-  setTimerRepository: (timerRepository: TimerRepository) => IApiBuilder
-  setTaskRepository: (taskRepository: TaskRepository) => IApiBuilder
-  setUserRepository: (userRepository: UserRepository) => IApiBuilder
-  build: () => any
-}
+export * from '@application/api'
