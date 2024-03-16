b174ea89d3813ab48d88fd7afb63466129a26709
Franco de Desarrollo <francoegi01@gmail.com>
2024-03-15 23:06:46 -0300

    refactor(domain/project): migrate to package workspace

---
+++ packages/domain/project/src/event/project-sum-pomodoro-count.ts
@@ -0,0 +1,9 @@
+import { DomainEvent, type DomainEventProps } from '@domain/core'
+
+export class ProjectSumPomodoroCountEvent extends DomainEvent {
+  actCounter: number
+  constructor(props: DomainEventProps<ProjectSumPomodoroCountEvent>) {
+    super(props)
+    this.actCounter = props.actCounter
+  }
+}
---
+++ packages/domain/project/src/project.exceptions.ts
@@ -0,0 +1,15 @@
+import { type ZodIssue } from 'zod'
+import { NotFound, ValidationError } from '@domain/core'
+import { type Project } from './project.entity'
+
+export class InvalidProject extends ValidationError {
+  constructor(issues?: ZodIssue[]) {
+    super('Invalid project', issues)
+  }
+}
+
+export class ProjectNotFound extends NotFound {
+  constructor(value: string, field: keyof Project | keyof Project['props'] = 'id') {
+    super(`Project with ${field}: ${value} not found`)
+  }
+}
