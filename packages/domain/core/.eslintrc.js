1b065f9d109bd6d7a0bf5a5043e4957210465568
Franco de Desarrollo <francoegi01@gmail.com>
2024-03-16 12:57:18 -0300

    chore(domain/core): rm useless comments

--- packages/domain/core/.eslintrc.js
+++ packages/domain/core/.eslintrc.js
@@ -1,26 +1,9 @@
 module.exports = {
-  // env: {
-  //   browser: true,
-  //   es2021: true,
-  //   node: true
-  // },
   root: true,
   extends: ['@config/eslint'].map(require.resolve),
-  // parserOptions: {
-  //   ecmaVersion: 'latest',
-  //   sourceType: 'module'
-  // },
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: './tsconfig.json'
   },
-  // ignorePatterns: ['node_modules/', 'dist/', 'build/']
-  rules: {
-    // "import/export": "off",
-    //   '@typescript-eslint/space-before-function-paren': 'off',
-    //   '@typescript-eslint/no-misused-promises': 'off',
-    //   '@typescript-eslint/indent': 'off',
-    //   '@typescript-eslint/brace-style': 'off',
-    //   '@typescript-eslint/member-delimiter-style': 'off'
-  }
+  rules: {}
 }
