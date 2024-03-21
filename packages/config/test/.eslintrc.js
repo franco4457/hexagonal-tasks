3e44ae3b3b2ab88bdeb665e0db664c35377fca67
Franco de Desarrollo <francoegi01@gmail.com>
2024-03-21 20:58:01 -0300

    feat(config/test): add package for testing
    - add bin for exec testing and utils for testing

---
+++ packages/config/test/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  extends: ['@config/eslint'].map(require.resolve),
+  parserOptions: {
+    include: ['*.ts', '*.tsx', '*.d.ts'],
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json']
+  },
+  ignorePatterns: ['vitest.config.ts']
+}
---
+++ packages/config/test/utils/constants.ts
@@ -0,0 +1,9 @@
+import { EventEmitter2 } from 'eventemitter2'
+
+export const TEST_ID = 'c2d7e0e0-4e0a-4b7a-8c7e-2a9a9b0a3b1a'
+export const REPO_CONFIG = {
+  appContext: 'TEST',
+  eventEmitter: new EventEmitter2()
+}
+export const MOCK_TOKEN =
+  'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImMyZDdlMGUwLTRlMGEtNGI3YS04YzdlLTJhOWE5YjBhM2IxYSIsImlhdCI6MTcwNzc4NTM0N30.F-sCWFjQ8DzpkRYjzlUPBgt70KfIR0ANwNuxKGw88DE'
