92135ea77932d4b7c2d7209adc69126b0b7b3664
Franco de Desarrollo <francoegi01@gmail.com>
2024-03-23 13:58:53 -0300

    fix(apps/express): catch via field if is a Validation Error

--- apps/express/src/errors.ts
+++ apps/express/src/errors.ts
@@ -1,15 +1,21 @@
-import { DomainError, ValidationError } from '@domain/core'
+import { type DomainError, type ValidationError } from '@domain/core'
 import { type ErrorRequestHandler } from 'express'
 
 export const mainErrorHanlder: ErrorRequestHandler = (err, _req, res, _next) => {
-  if (!(err instanceof DomainError)) {
+  if (err.statusCode == null || err.name == null) {
     res.status(500).json({ error: true, message: 'Unhandled internal error' })
     return
   }
-  const { statusCode, ...resErr } = err
+  const {
+    statusCode,
+    IS_VALIDATION_ERROR = false,
+    ...resErr
+  } = err as DomainError & { IS_VALIDATION_ERROR?: boolean }
+
   res.status(statusCode)
-  if (err instanceof ValidationError) {
-    res.json({ error: true, ...resErr, errors: JSON.parse(err.message), message: err.name })
+  if (IS_VALIDATION_ERROR) {
+    const e = err as ValidationError
+    res.json({ error: true, ...resErr, errors: JSON.parse(e.message), message: e.name })
     return
   }
   res.json({ error: true, ...resErr, message: err.message })
