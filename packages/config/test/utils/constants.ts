c436ae1e4fff3de5c77ab7077f8b42efb8dfdd4e
Franco de Desarrollo <francoegi01@gmail.com>
2024-03-26 20:12:13 -0300

    fix(config/test): use in memory event bus into repo config

--- packages/config/test/utils/constants.ts
+++ packages/config/test/utils/constants.ts
@@ -1,9 +1,9 @@
-import { EventEmitter2 } from 'eventemitter2'
+import { InMemoryEventBus } from '@infrastructure/event-bus-in-memory'
 
 export const TEST_ID = 'c2d7e0e0-4e0a-4b7a-8c7e-2a9a9b0a3b1a'
 export const REPO_CONFIG = {
   appContext: 'TEST',
-  eventEmitter: new EventEmitter2()
+  eventBus: new InMemoryEventBus()
 }
 export const MOCK_TOKEN =
   'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImMyZDdlMGUwLTRlMGEtNGI3YS04YzdlLTJhOWE5YjBhM2IxYSIsImlhdCI6MTcwNzc4NTM0N30.F-sCWFjQ8DzpkRYjzlUPBgt70KfIR0ANwNuxKGw88DE'
